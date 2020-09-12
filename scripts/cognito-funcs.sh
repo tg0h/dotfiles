337849f5708fb9638e96e43530ab2208af140dd2
timtestallthethings <timtestallthethings@gmail.com>
2020-09-12 19:32:24 +0800

    cognito script - get sign ins, implement error retry (on rate exceeded)
    use pipestatus to detect errors in the pipeline. the shebang is important so the shell knows to use sh or zsh

---
+++ scripts/cognito-funcs.sh
@@ -0,0 +1,12 @@
+#!/usr/local/bin/zsh
+#this uses the homebrew version of zsh stored in /usr/local/bin
+#for even more portability, use /usr/bin/env zsh
+
+#hack place statment before condition in while loop. lol!
+# set -o pipefail - this is an alternative to pipestatus
+while
+  aws cognito-idp admin-list-user-auth-events --user-pool-id $_CERTIFY_POOL_ID --username SG$1 2>/dev/null | jq --raw-output ".AuthEvents[0] | [$1,.EventResponse,.EventType,.CreationDate,.EventRisk.RiskDecision,.EventRisk.CompromisedCredentialsDetected,.ChallengeResponses[0].ChallengeName,.ChallengeResponses[0].ChallengeResponse] | @csv"
+  [[ ${pipestatus[1]} -ne 0 ]] #if the 1st stage of the pipe fails, try again
+do
+  continue
+done
