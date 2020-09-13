f664dfc448e57c9e932ada6455889f152df2f15f
timtestallthethings <timtestallthethings@gmail.com>
2020-09-14 00:06:52 +0800

    add commented function for debugging (does not suppress stderr)

--- scripts/cognito-funcs.sh
+++ scripts/cognito-funcs.sh
@@ -5,7 +5,8 @@
 #hack place statment before condition in while loop. lol!
 # set -o pipefail - this is an alternative to pipestatus
 while
-  aws cognito-idp admin-list-user-auth-events --user-pool-id $_CERTIFY_POOL_ID --username SG$1 2>/dev/null | jq --raw-output ".AuthEvents[0] | [$1,.EventResponse,.EventType,.CreationDate,.EventRisk.RiskDecision,.EventRisk.CompromisedCredentialsDetected,.ChallengeResponses[0].ChallengeName,.ChallengeResponses[0].ChallengeResponse] | @csv"
+  aws cognito-idp admin-list-user-auth-events --user-pool-id $_CERTIFY_POOL_ID --username $1 2>/dev/null | jq --raw-output ".AuthEvents[0] | [\"$1\",.EventResponse,.EventType,.CreationDate,.EventRisk.RiskDecision,.EventRisk.CompromisedCredentialsDetected,.ChallengeResponses[0].ChallengeName,.ChallengeResponses[0].ChallengeResponse] | @csv"
+  # aws cognito-idp admin-list-user-auth-events --user-pool-id $_CERTIFY_POOL_ID --username $1 | jq --raw-output ".AuthEvents[0] | [\"$1\",.EventResponse,.EventType,.CreationDate,.EventRisk.RiskDecision,.EventRisk.CompromisedCredentialsDetected,.ChallengeResponses[0].ChallengeName,.ChallengeResponses[0].ChallengeResponse] | @csv"
   [[ ${pipestatus[1]} -ne 0 ]] #if the 1st stage of the pipe fails, try again
 do
   continue
