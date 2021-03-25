91608cd5856f307349154527be711ea1d67e8295
timtestallthethings <timtestallthethings@gmail.com>
2021-03-25 08:01:28 +0800

    feat(ios): add idevicename

--- zsh_ios.zsh
+++ zsh_ios.zsh
@@ -1,8 +1,20 @@
 # install ios ipa
+# see https://libimobiledevice.org/
 
 function idd() {
   idevice_id
 }
+
+function idn() {
+  # while getopts 'l:e:a:ds:' opt; do
+  #   case "$opt" in
+  #     d) usbMode="-d" ;;
+  #   esac
+  # done
+  # shift $(($OPTIND - 1))
+  idevicename
+}
+
 
 function idi() {
   # while getopts 'l:e:a:ds:' opt; do
