f66c1bacc4a3454165df04c8bb3a0f322214d056
timtestallthethings <timtestallthethings@gmail.com>
2021-03-27 12:54:11 +0800

    feat(cli-date): add gn function to set now global var

--- zsh_funcs.zsh
+++ zsh_funcs.zsh
@@ -4,3 +4,13 @@ function rgs(){ rg $1 --sortr created }
 function ffm() {
   ffmpeg -i $1 -vcodec libx265 -crf 28 "$1"-out.mp4
 }
+
+
+function gn(){
+# set now to YYYY-MM-DD_THHMM
+#see http://zsh.sourceforge.net/Doc/Release/User-Contributions.html for docs on zmv
+now=$(date +%Y-%m-%d_T%H%M)
+
+# # -C forces cp, ln or mv
+# zmv -C '*' './$f-$now'
+}
