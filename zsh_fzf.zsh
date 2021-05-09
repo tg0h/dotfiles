fde36dd061ec5d36936cb0cbd4275d7c845e0c8c
tim <timothysl_goh@certisgroup.com>
2021-05-09 11:11:19 +0800

    doc: tidy up fzf comments
    how to set up mvim command

--- zsh_fzf.zsh
+++ zsh_fzf.zsh
@@ -1,14 +1,11 @@
-#use rg for fzf default search
-# --hidden -include hidden files
-# --follow -include symbolic links
-# --no-ignore-vcs - include vcs files
-#export FZF_DEFAULT_COMMAND='rg --files --hidden --follow --no-ignore-vcs'
-FD_OPTIONS="--follow --hidden --exclude .git --exclude node_modules"
-#show fzf options top down instead of bottom up
-#use height of 40%
-#use highlight to preview file with colour
-#export FZF_DEFAULT_OPTS="--height 40% --layout=reverse --border"
-#turn off preview window by adding right:hidden:wrap
+# NOTE the key bindings, ctrl e, ctrl v etc
+# to get mvim on the command line, brew install macvim
+
+# show fzf options top down instead of bottom up
+# use height of 40%
+# use highlight to preview file with colour
+# export FZF_DEFAULT_OPTS="--height 40% --layout=reverse --border"
+# turn off preview window by adding right:hidden:wrap
 export FZF_DEFAULT_OPTS="
 --no-mouse
 --height 50%
@@ -30,6 +27,11 @@ export FZF_DEFAULT_OPTS="
 
 #export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
 export FZF_DEFAULT_COMMAND="git ls-files --cached --others --exclude-standard | fd --type f --type l $FD_OPIONS"
+# --hidden -include hidden files
+# --follow -include symbolic links
+# --no-ignore-vcs - include vcs files
+#export FZF_DEFAULT_COMMAND='rg --files --hidden --follow --no-ignore-vcs'
+FD_OPTIONS="--follow --hidden --exclude .git --exclude node_modules"
 
 #search to exclude .git and node_modules
 alias fzfi='rg --files --hidden --follow --no-ignore-vcs -g "!{node_modules,.git}" | fzf'
