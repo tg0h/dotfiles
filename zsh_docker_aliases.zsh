b43df729a0998f99b099964be5b43281e2fcd799
tim <timothysl_goh@certisgroup.com>
2021-06-05 15:52:17 +0800

    docker: add docker aliases

--- zsh_docker_aliases.zsh
+++ zsh_docker_aliases.zsh
@@ -3,12 +3,20 @@ alias dc='docker container'
 
 alias dca='docker container attach'
 
+## EXEC
 alias dce='docker container exec'
+alias dceit='docker container exec -it'
+alias dceh='docker container exec --help'
 
+## LOGS
 alias dcg='docker container logs'
 alias dcgt='docker container logs --tail'
 
+## INSPECT
 alias dci='docker container inspect'
+
+## PORT
+alias dco='docker container port'
 
 
 ## LIST
@@ -19,7 +27,9 @@ alias dcla='docker container ls -a'
 alias dcm='docker container rm '
 alias dcma='docker container rm $(docker container ls -aq)'
 
+## RUN
 alias dcr='docker container run '
+alias dcrit='docker container run -it'
 alias dcrd='docker container run -d'
 alias dcrdn='docker container run -d --name'
 
