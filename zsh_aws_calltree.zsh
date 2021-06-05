49efee1948059a35ced21d24e1be598427af5caf
tim <timothysl_goh@certisgroup.com>
2021-06-05 15:50:34 +0800

    calltree: add docs

--- zsh_aws_calltree.zsh
+++ zsh_aws_calltree.zsh
@@ -8,14 +8,22 @@ function ctfilter(){
 
 # TODO: how to remove target and delete rule in a single function?
 # delete the cloudwatch rule target before deleting it
+# run ctfilter ... | ctdelt, then run
+# run ctfilter ... | ctdelr
 function ctdelt(){
-   # to use, run this command: ctfilter 2020 | ctdelt
+   # examples:
+   # ctfilter 2020 | ctdelt
+   # ctfilter 202103 | ctdelt
    parallel aws events remove-targets --rule {} --ids 1
    # parallel aws events delete-rule --name {}
 }
 
 # delete the cloudwatch rule after deleting the cloudwatch rule target
 function ctdelr(){
+   # examples:
+   # ctfilter 2020 | ctdelr
+   # ctfilter 202103 | ctdelr
    # parallel aws events remove-targets --rule {} --ids 1
+   # to use, run this command: ctfilter 2020 | ctdelr
    parallel aws events delete-rule --name {}
 }
