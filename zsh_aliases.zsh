e127cc0375d5c776c47b39b841c5186d8b3a0a42
tim <timothysl_goh@certisgroup.com>
2021-06-29 19:18:53 +0800

    config: add zt alias, remove unused aliases

--- zsh_aliases.zsh
+++ zsh_aliases.zsh
@@ -21,7 +21,6 @@ alias zshn='vim ~/.zsh_node.zsh'
 # tools
 alias zshfzf='vim ~/.zsh_fzf.zsh' 
 
-
 # git
 alias zshgit='vim ~/.zsh_git_aliases.zsh'
 alias zshgitf='vim ~/.zsh_fzf_git.zsh'
@@ -42,26 +41,21 @@ alias l='lsd -lah' #use lsd to supercharge ls output
 alias ll='lsd -lh' #use lsd to supercharge ls output
 alias lt="lsd -laht"
 
-# quick print eval loop
+# quick print eval loop ====================================================
 # source
 alias ss="source $HOME/dotfiles/scripts/scratch"
 alias sss="source $HOME/.zshrc"
 # edit
 alias ee="vim $HOME/dotfiles/scripts/scratch"
 
-
 #zsh jira plugin
 alias j=jira
+alias zt="cd /tmp"
 alias ag="alias | grep"
 alias sg="set | grep"
 alias fork="open . -a /Applications/Fork.app"
-alias kraken="open . -a /Applications/GitKraken.app"
-alias topten="history | commands | sort -rn | head"
 alias myip="curl http://ipecho.net/plain; echo"
 alias dirs='dirs -v | head -10'
-alias usage='du -h -d1'
 # alias update="source ~/.zshrc" #doesn't work - complains about some fd error
-alias sshdir="cd ~/.ssh"
-alias runp="lsof -i " #eg runp :1234 tells you whether port 1234 is being used
 alias vpn="networksetup -connectpppoeservice 'edo vpn'"
 alias dvpn="networksetup -disconnectpppoeservice 'edo vpn'"
