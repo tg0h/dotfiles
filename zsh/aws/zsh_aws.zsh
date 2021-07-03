149bf0a080ca275f2a24ee488882d1603bf7b0cc
tim <timothysl_goh@certisgroup.com>
2021-07-03 16:34:27 +0800

    feat(aws): add aws switcher
    I can source my own env files when using my own switcher

--- zsh/aws/zsh_aws.zsh
+++ zsh/aws/zsh_aws.zsh
@@ -1,4 +1,55 @@
 as(){
+  local profile
+  case $1 in
+    d)
+      . ~/.aws/aws.argus.develop.env
+      . ~/.argus/argus.develop.env
+      asp adev
+      ;;
+    s)
+      . ~/.aws/aws.argus.develop.env
+      . ~/.argus/argus.staging.env
+      asp adev
+      ;;
+    p)
+      . ~/.aws/aws.argus.production.env
+      . ~/.argus/argus.production.env
+      asp aprod
+      ;;
 
+    as)
+      . ~/.aws/argusAu.staging.env
+      . ~/.argus/argusAu.staging.env
+      asp aastg
+      ;;
+    ap)
+      . ~/.aws/argusAu.production.env
+      . ~/.argus/argusAu.production.env
+      asp aaprod
+      ;;
+
+    cd)
+      . ~/.aws/aws.certify.develop.env
+      . ~/.certify/certify.develop.env
+      asp cdev
+      ;;
+    cs)
+      . ~/.aws/aws.certify.develop.env
+      . ~/.certify/certify.staging.env
+      asp cdev
+      ;;
+    cp)
+      . ~/.aws/aws.certify.production.env
+      . ~/.certify/certify.production.env
+      asp cprod
+      ;;
+
+    sb)
+      . ~/.aws/aws.sandbox.env
+      asp sb
+      ;;
+    *)
+      echo "${fg[red]} no profile found ${reset_color}"
+  esac
 }
 
