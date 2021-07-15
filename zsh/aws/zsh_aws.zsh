bfab242f83034dab2c3b2cd6a83c729971ec2ce9
tim <timothysl_goh@certisgroup.com>
2021-07-15 08:17:15 +0800

    feat(aws): add default region to env for switcher

--- zsh/aws/zsh_aws.zsh
+++ zsh/aws/zsh_aws.zsh
@@ -2,49 +2,67 @@ as(){
   local profile
   case $1 in
     d)
+      export AWS_DEFAULT_REGION=ap-southeast-1
+      #stores cloudwatch cache log name
       . ~/.aws/aws.argus.develop.env
+
+      # argus ssh and rds settings
       . ~/.argus/argus.develop.env
+
+      . ~/.abs/argus.develop.env
       asp adev
       ;;
     s)
+      export AWS_DEFAULT_REGION=ap-southeast-1
       . ~/.aws/aws.argus.develop.env
       . ~/.argus/argus.staging.env
+      . ~/.abs/argus.staging.env
       asp adev
       ;;
     p)
+      export AWS_DEFAULT_REGION=ap-southeast-1
       . ~/.aws/aws.argus.production.env
       . ~/.argus/argus.production.env
+      . ~/.abs/argus.production.env
       asp aprod
       ;;
 
     as)
-      . ~/.aws/argusAu.staging.env
+      export AWS_DEFAULT_REGION=ap-southeast-2
+      . ~/.aws/aws.argus.au.staging.env
       . ~/.argus/argusAu.staging.env
+      # . ~/.abs/argus.au.staging.env
       asp aastg
       ;;
     ap)
-      . ~/.aws/argusAu.production.env
+      export AWS_DEFAULT_REGION=ap-southeast-2
+      . ~/.aws/aws.argus.au.production.env
       . ~/.argus/argusAu.production.env
+      # . ~/.abs/argus.au.production.env
       asp aaprod
       ;;
 
     cd)
+      export AWS_DEFAULT_REGION=ap-southeast-1
       . ~/.aws/aws.certify.develop.env
       . ~/.certify/certify.develop.env
       asp cdev
       ;;
     cs)
+      export AWS_DEFAULT_REGION=ap-southeast-1
       . ~/.aws/aws.certify.develop.env
       . ~/.certify/certify.staging.env
       asp cdev
       ;;
     cp)
+      export AWS_DEFAULT_REGION=ap-southeast-1
       . ~/.aws/aws.certify.production.env
       . ~/.certify/certify.production.env
       asp cprod
       ;;
 
     sb)
+      export AWS_DEFAULT_REGION=ap-southeast-1
       . ~/.aws/aws.sandbox.env
       asp sb
       ;;
