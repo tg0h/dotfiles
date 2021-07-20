6b4d88c0b58af146e80fc983627049659926a657
tim <timothysl_goh@certisgroup.com>
2021-07-20 12:27:14 +0800

    config(kitty): zshk shortcut to edit kitty config

--- zsh/zsh_aliases.zsh
+++ zsh/zsh_aliases.zsh
@@ -5,46 +5,48 @@ alias -g G='| gron'
 
 # =========================================================================
 # edit config file shortcuts
-alias zshe='vim ~/.env'
-alias zshrc='vim ~/dotfiles/zshrc'
+alias zshe='nvim ~/.env'
+alias zshrc='nvim ~/dotfiles/zshrc'
 
-alias zshaa='vim ~/.zsh/zsh_aliases.zsh'
-alias zshf='vim ~/.zsh/zsh_funcs.zsh' #misc functions, use zsh_functions folder for more organization
+alias zshaa='nvim ~/.zsh/zsh_aliases.zsh'
+alias zshf='nvim ~/.zsh/zsh_funcs.zsh' #misc functions, use zsh_functions folder for more organization
 
-alias zshw='vim ~/.ideavimrc' # the webstorm ideavim rc file
+alias zshw='nvim ~/.ideanvimrc' # the webstorm ideanvim rc file
+
+alias zshk='nvim ~/.config/kitty/kitty.conf' # the kitty config file
 
 # mobile
-alias zsha='vim ~/.zsh/zsh_android.zsh'
-alias zshi='vim ~/.zsh/zsh_ios.zsh'
+alias zsha='nvim ~/.zsh/zsh_android.zsh'
+alias zshi='nvim ~/.zsh/zsh_ios.zsh'
 
 # dev
-alias zshd='vim ~/.zsh/zsh_docker_aliases.zsh'
-alias zshn='vim ~/.zsh/zsh_node.zsh'
+alias zshd='nvim ~/.zsh/zsh_docker_aliases.zsh'
+alias zshn='nvim ~/.zsh/zsh_node.zsh'
 
 # tools
-alias zshfzf='vim ~/.zsh/zsh_fzf.zsh' 
+alias zshfzf='nvim ~/.zsh/zsh_fzf.zsh' 
 
 # git
-alias zshgit='vim ~/.zsh/zsh_git_aliases.zsh'
-alias zshgitf='vim ~/.zsh/zsh_fzf_git.zsh'
+alias zshgit='nvim ~/.zsh/zsh_git_aliases.zsh'
+alias zshgitf='nvim ~/.zsh/zsh_fzf_git.zsh'
 
 # devops, deployment
-alias zshj='vim ~/.zsh/zsh_jira.zsh'
-alias zshg='vim ~/.zsh/zsh_gitlab.zsh'
-alias zshac='vim ~/.zsh/zsh_appCenter.zsh'
+alias zshj='nvim ~/.zsh/zsh_jira.zsh'
+alias zshg='nvim ~/.zsh/zsh_gitlab.zsh'
+alias zshac='nvim ~/.zsh/zsh_appCenter.zsh'
 
 # aws
-alias zshrds='vim ~/.zsh/aws/zsh_aws_rds.zsh'
-alias zshcw='vim ~/.zsh/aws/zsh_aws_cloudwatch.zsh'
-alias zshc='vim ~/.zsh/aws/zsh_aws_cognito.zsh'
-alias zshct='vim ~/.zsh/aws/zsh_aws_calltree.zsh'
+alias zshrds='nvim ~/.zsh/aws/zsh_aws_rds.zsh'
+alias zshcw='nvim ~/.zsh/aws/zsh_aws_cloudwatch.zsh'
+alias zshc='nvim ~/.zsh/aws/zsh_aws_cognito.zsh'
+alias zshct='nvim ~/.zsh/aws/zsh_aws_calltree.zsh'
 
 # quick print eval loop ====================================================
 # source
 alias ss="source $HOME/dotfiles/scripts/scratch"
 alias sss="source $HOME/.zshrc"
 # edit
-alias ee="vim $HOME/dotfiles/scripts/scratch"
+alias ee="nvim $HOME/dotfiles/scripts/scratch"
 
 # others ====================================================================
 alias norg="gron --ungron"
@@ -66,5 +68,5 @@ alias vpn="networksetup -connectpppoeservice 'edo vpn'"
 alias dvpn="networksetup -disconnectpppoeservice 'edo vpn'"
 # exa
 alias exat="exa --tree --long --icons --git"
-# vim
+# nvim
 alias eu="nvim"
