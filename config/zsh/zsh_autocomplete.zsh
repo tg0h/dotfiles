8e2a49bcd74c6d96c661202fc4cee9e7d6504c7a
tim <timothysl_goh@certisgroup.com>
2021-08-15 14:19:08 +0800

    config: clean up zsh-autocomplete settings

--- config/zsh/zsh_autocomplete.zsh
+++ config/zsh/zsh_autocomplete.zsh
@@ -1,8 +1,16 @@
-# make zsh-autocomplete stay silent until 3 chars have been typed
-zstyle ':autocomplete:*' min-input 3  # number of characters (integer)
+# https://github.com/marlonrichert/zsh-autocomplete/blob/main/.zshrc# make zsh-autocomplete stay silent until 3 chars have been typed
+
+# Wait this many seconds for typing to stop, before showing completions.
+# zstyle ':autocomplete:*' min-delay 0.1  # float
+
+zstyle ':autocomplete:*' min-input 1  # number of characters (integer)
 
 #space does history expansion, does not do spelling correction
-zstyle ':autocomplete:space:*' magic expand-history
+# zstyle ':autocomplete:space:*' magic expand-history
+
+# If there are fewer than this many lines below the prompt, move the prompt up
+# to make room for showing this many lines of completions (approximately).
+# zstyle ':autocomplete:*' list-lines 16
 
 #always show groups
 # zstyle ':autocomplete:*' groups always
@@ -11,7 +19,7 @@ zstyle ':autocomplete:space:*' magic expand-history
 #zstyle ':autocomplete:*' frecent-dirs off
 
 #tab starts menu selection instead of completing the closest match
-zstyle ':autocomplete:tab:*' completion insert
+# zstyle ':autocomplete:tab:*' completion insert
 #zstyle ':autocomplete:tab:*' completion fzf
 #
 
