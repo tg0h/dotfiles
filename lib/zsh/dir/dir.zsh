4011b2f1c7e3a3213d357b3e2e6413f88f4164c4
tim goh <timothysl_goh@certisgroup.com>
2021-09-19 14:50:42 +0800

    fix(dir):add back udc etc aliases

--- lib/zsh/dir/dir.zsh
+++ lib/zsh/dir/dir.zsh
@@ -14,3 +14,14 @@ function lt1(){
     cp $latestFile .
     echo copied $fg[green]$latestFile$reset_color here
 }
+
+alias uda='cd $XDl_CACHE_HOME'
+alias udt='cd $XDG_DATA_HOME'
+alias uds='cd $XDG_STATE_HOME'
+alias udb='cd ~/.local/bin'
+alias udl='cd ~/.local/lib'
+
+function udc(){
+  # switch to my config folder
+  cd $XDG_CONFIG_HOME
+}
