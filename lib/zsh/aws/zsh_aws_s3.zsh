6f796c5a1efd0af0ecfaafd24c83944f50be8c48
tim goh <timothysl_goh@certisgroup.com>
2021-10-09 10:36:04 +0800

    feat(aws-s3): add stl s3 ls function

--- lib/zsh/aws/zsh_aws_s3.zsh
+++ lib/zsh/aws/zsh_aws_s3.zsh
@@ -1,4 +1,6 @@
 function std(){
+  # download face picture from argus s3 bucket and open it
+  # std <facePath>
 #TODO: use redis?
 
   local s3path
@@ -14,4 +16,8 @@ function std(){
 
   aws s3 cp s3://$ARGUS_S3_FACE/$s3path /tmp/
   open /tmp/$filename
+}
+function stl(){
+  # aws s3 ls
+  aws s3 ls
 }
