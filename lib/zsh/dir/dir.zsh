a32049aa9b627fcb3ca8e3907b1e8a3b39432657
tim goh <timothysl_goh@certisgroup.com>
2021-10-10 12:05:14 +0800

    feat(dir): display configuration with fzf preview (1h)
    use fzf preview: you can determine the fixed header rows, the original
    scroll position, and a further offset to the scroll position

--- lib/zsh/dir/dir.zsh
+++ lib/zsh/dir/dir.zsh
@@ -16,6 +16,22 @@ function lt1(){
     echo copied $fg[green]$latestFile$reset_color here
   }
 
+function udConfigureList(){
+  local configFile=$1
+
+  # --highlight-line
+  #   this is a bat feature
+  # --preview window ~8,+{1}-5
+  #   this is a fzf feature
+  #   ~8 - show first 8 lines (header)
+  #   +{1} - fzf delimits the input piped in to it and provides access via index variables {n}. 
+  #   the default delimiter fzf uses is space but can be specified via --delimiter <delimiter>
+  #   pass the first index variable from bat (which is the line number)
+  #   the number is signed, you can show eg the +n row or the -n row (the nth row from the bottom)
+  #   -5 subtract 5 rows (go up 5 rows) so that you don't show the highlighted line as the first line
+  #   since you want to provide context by showing the rows above the highlighted line
+  bat --style=numbers --color always $configFile | fzf --preview 'bat --style=numbers --color=always '"$configFile"' --highlight-line {1}' --preview-window ~8,+{1}-5
+}
 
 function ud(){
   # fzf preview and switch to folder
@@ -26,7 +42,10 @@ function ud(){
   # eg ud ~/.local/bin 2
   local configFile=$XDG_CONFIG_HOME/zsh/dir/config.yml
 
-  [[ $1 == "configure" ]] && [[ $2 != "edit" ]] && yq eval '.keymaps' $configFile --colors | fzf && return
+  if [[ $1 == "configure" ]] && [[ $2 == "list" ]]; then
+    udConfigureList $configFile
+    return
+  fi
 
   # prefer nvim over eu for clarity and unneeded dependency on eu alias
   [[ $1 == "configure" ]] && [[ $2 == "edit" ]] && nvim $configFile && return
