5b8a0895d608db219a0187788e7a608f314e0713
tim goh <timothysl_goh@certisgroup.com>
2021-10-15 07:40:15 +0800

    feat(jira): parse mention message (2h)
    get the app name, url, commit/merge request and its hash/merge request
    number. use regex negative look ahead/look behind

--- lib/zsh/jira/issues.zsh
+++ lib/zsh/jira/issues.zsh
@@ -54,17 +54,39 @@ function rai(){
                           subtasks: (.fields.subtasks | map (.key)),
                           git: (.fields.comment.comments | 
                                 map(select(.author.name == "certis.ads")) |
-                                map(.body |
-                                      # jq uses the oniguruma regex engine https://github.com/kkos/oniguruma/blob/master/doc/RE
-                                      # to escape a word class, eg \w, use \\w
-                                      # however, since we are running this from a terminal, we need to escape again.
-                                      # hence \\ becomes \\\\ . fun
-                                      # +? is non greedy it matches conservatively until it finds the next expression
-                                      # eg (.+?)(|) if there are several pipes | in the string, the nongreedy expression matches
-                                      # everything until the first pipe. the greedy expression matches everything until the last pipe.
-                                      # (?<group>(exp) is a jq named capturing group
+                                map(
+                                      {
+                                      created,
+                                      mention: .body |
+                                              # jq uses the oniguruma regex engine https://github.com/kkos/oniguruma/blob/master/doc/RE
+                                              # to escape a word class, eg \w, use \\w
+                                              # however, since we are running this from a terminal, we need to escape again.
+                                              # hence \\ becomes \\\\ . fun
+                                              # +? is non greedy it matches conservatively until it finds the next expression
+                                              # eg (.+?)(|) if there are several pipes | in the string, the nongreedy expression matches
+                                              # everything until the first pipe. the greedy expression matches everything until the last pipe.
+                                              # (?<group>(exp) is a jq named capturing group
 
-                                      capture("(\\\\[)(?<name>.+?)(\\\\|)(.+?)(\\\\|)(?<url>.+?)(\\\\])") | .url
+
+                                              # capture("(\\\\[)(?<name>.+?)(\\\\|)(.+?)(\\\\|)(?<url>.+?)(\\\\])") | .url,
+                                              capture("\\\\[(?<userName>.*?)\\\\|(?<userUrl>.*?)].*\\\\[a (?<mentionType>.*?) of(?<appName>.*?)\\\\|(?<mentionUrl>.*?)](?<test>:\n')(?<mentionDescription>.*)'") |
+                                              {
+                                                userName, 
+                                                # userUrl,
+                                                mentionType,
+                                                appName,
+                                                mentionUrl,
+                                                # parse the mentionUrl string https://git.ads.certis.site/optimax/optimax-apps/optimax-officer-android/-/commit/b27ff0a4c50b21444c10cef1aa0eb82c426ea727
+                                                # ?= is positive lookahead match but don't capture
+                                                # appUrl: (.mentionUrl | match("https.*(?=/-)")| .string), 
+                                                # ?<= is negative look behind
+                                                # mentionTypeSlug: (.mentionUrl | match("(?<=-/).*?(?=/)") | .string), 
+                                                # ?! is negative look behind. the ?! expression should not match
+                                                # in other words, get the last "segment" which follows after the last / in "https://blah/foo/segment"
+                                                mentionRef: (.mentionUrl | match("(?!.*/).*$") | .string), 
+                                                mentionDescription
+                                              },
+                                      }
                                    )
                                ),
                           # for merge commit comments, look for author certis.ads
@@ -75,3 +97,45 @@ EOF
       jq "$jqQuery" <<< $ticket
     fi
   }
+
+function raib(){
+  # get jira issue brief
+  # rai 1234 gets rai ARG-1234
+      # heredoc redirects doc to cat input, unable to redirect directly into a variable
+      # the hypen <<- allows you to indent your heredoc
+      # not sure why # is allowed in the jqQuery ¯\_(ツ)_/¯
+    local ticket=$(https -b -a $JIRA_SECRET $JIRA_URL/rest/agile/1.0/issue/ARG-$1)
+
+      local jqQuery=$(cat <<-EOF
+                        {
+                          key: .key,
+                          summary: .fields.summary,
+                          sprint: .fields.sprint.name,
+                          components: .fields.components[0]?.name,
+                          epicname: .fields.epic.name,
+                          status: .fields.status.name,
+                          created: .fields.created,
+                          reporter: .fields.reporter.name, 
+                          assignee: .fields.assignee.name,
+                          git: (.fields.comment.comments | 
+                                map(select(.author.name == "certis.ads")) |
+                                map(
+                                      {
+                                      created,
+                                      mention: .body |
+                                              capture("\\\\[(?<userName>.*?)\\\\|(?<userUrl>.*?)].*\\\\[a (?<mentionType>.*?) of(?<appName>.*?)\\\\|(?<mentionUrl>.*?)](?<test>:\n')(?<mentionDescription>.*)'") |
+                                              {
+                                                userName, 
+                                                mentionUrl,
+                                                mentionRef: (.mentionUrl | match("(?!.*/).*$") | .string), 
+                                                mentionDescription
+                                              },
+                                      }
+                                   )
+                               ),
+                        }
+EOF
+)
+      jq "$jqQuery" <<< $ticket
+  }
+
