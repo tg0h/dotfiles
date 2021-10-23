7128d5a61bdf21472c7c420249d4f866e8bc0c59
tim goh <timothysl_goh@certisgroup.com>
2021-10-23 17:52:29 +0800

    config(jira): use new api url

---
+++ config/zsh/jira.zsh
@@ -0,0 +1,8 @@
+# for the zsha jira plugin
+# also used by my jira cli lib
+export JIRA_URL='https://certisgroup.atlassian.net'
+export JIRA_API_URL='https://certisgroup.atlassian.net/rest/agile/1.0'
+export JIRA_PREFIX='ARG-'
+export JIRA_NAME='***REMOVED***'
+export JIRA_RAPID_BOARD=true
+
--- config/zsh/zsh_jira.plug.zsh
+++
@@ -1,5 +0,0 @@
-export JIRA_URL='https://jira.ads.certis.site'
-export JIRA_PREFIX='ARG-'
-export JIRA_NAME='***REMOVED***'
-export JIRA_RAPID_BOARD=true
-
