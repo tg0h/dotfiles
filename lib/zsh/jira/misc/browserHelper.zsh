ad81c502dfeddf701df5fbfd906aec9cba90b186
tim goh <timothysl_goh@certisgroup.com>
2021-11-01 08:55:44 +0800

    feat(jira): add jira open in browser functions

--- lib/zsh/jira/misc/browserHelper.zsh
+++ lib/zsh/jira/misc/browserHelper.zsh
@@ -1,4 +1,14 @@
-function f(){
-  # f 2805 opens ARG-2805 in jira in firefox
+function fa(){
+  # fa 2805 opens ARG-2805 in jira in firefox
   open -a firefox https://certisgroup.atlassian.net/browse/ARG-$1
+}
+
+function ff(){
+  # ff <arg> opens ACF-<arg> in jira in firefox
+  open -a firefox https://certisgroup.atlassian.net/browse/ACF-$1
+}
+
+function fo(){
+  # fo <arg> opens OTXSC-<arg> in jira in firefox
+  open -a firefox https://certisgroup.atlassian.net/browse/OTXSC-$1
 }
