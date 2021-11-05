2f8e21fd04796a1ffbaa01394c85213aa87344f8
tim goh <timothysl_goh@certisgroup.com>
2021-11-05 12:04:57 +0800

    remove scratch

--- lib/scratch.zsh
+++ lib/scratch.zsh
@@ -1,34 +1,21 @@
 #!/bin/zsh
 
-pp(){
-# for f in ~/.local/lib/zsh/***/*; 
-# do [[ -f $f ]] 
-#   echo $f
-#   . $f; 
-# done
-
-# for f in ~/.local/lib/abs/***/*.zsh; 
-# do [[ -f $f ]] 
-#   echo $f
-#   . $f; 
-# done
-}
-
 tt(){
-  # echo hello
-  # echo $_fzf_homeFn_dir
-  # rg \
-  #   --type zsh \
-  #   --color always \
-  #   --field-context-separator '' \
-  #   --no-context-separator \
-  #   # --only-matching -e '^\s*function\s+(?P<fname>([^\s(]+))' -r '$fname' -A 1 \
-  #   --only-matching  '^\s*function\s+' 
-  #   --field-match-separator ' ' \
-  #   $_fzf_homeFn_dir \
-    # | gsed -E 's!(\s*)(#)!\2!' \
-    # | gsed "s!$_fzf_homeFn_dir!!" \
-    # | gsed 'N;s/\n/ /' \
-    # | gawk '{$3="-"; print}' 
+  while getopts 'p:' opt; do
+    case "$opt" in
+      p) prefix=$OPTARG;;
+    esac
+  done
+  shift $(($OPTIND - 1))
+
+  echo args is $@
+
+  shift
+
+  echo args is $@
+
+  for ticket in "$@"; do
+    echo $ticket
+  done
 }
 
