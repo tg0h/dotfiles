cdb6764f4de1bd2e3218af25e89d43f38a5bf2ef
tim goh <timothysl_goh@certisgroup.com>
2021-11-11 08:03:38 +0800

    feat(utils): add command history (30m)

---
+++ lib/zsh/utilities/commandHistory.zsh
@@ -0,0 +1,25 @@
+# https://unix.stackexchange.com/questions/147563/how-do-i-repeat-the-last-command-without-using-the-arrow-keys
+function ao(){
+  # repeat the last command
+  # oops, it's much easier in zsh to just use vi mode, ESC-k
+  # (go to command mode and press k for up)
+  fc -e -
+}
+
+function aoe(){
+  # repeat the second last command
+  # oops, it's much easier in zsh to just use vi mode, ESC-k
+  # (go to command mode and press k for up)
+  fc -e - -2
+}
+
+function aoeu(){
+  # list previous commands
+  # note - running aoeu itself adds a command to the command history :|
+  fc -l
+}
+
+function fclf(){
+  # list commands with timestamp
+  fc -lf
+}
