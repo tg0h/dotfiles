520822739a4e25870e11cc978222f9f613bccf35
tim goh <timothysl_goh@certisgroup.com>
2021-11-13 16:56:31 +0800

    fix(jira): fix jira pagination if isLast is not returned (1h)

--- lib/zsh/jira/pagination.core.zsh
+++ lib/zsh/jira/pagination.core.zsh
@@ -20,12 +20,19 @@ function _jira_get_next_startAt(){
   # note that pagination for jira rest and jira service desk rest is different
   local response=$1
 
+  local maxResults startAt isLast nextStartAt total
   # TODO: this fails if the json response contains a newline
   # eg if the service desk ticket summary has a newline
   # jq complains with 'parse error: Invalid string: control characters from U+0000 through U+001F must be escaped at line 3, column 33'
+
+  # this returns the number of results for the current page
   maxResults=$(echo $response | jq '.maxResults')
-  startAt=$(echo $response | jq '.startAt')
-  isLast=$(echo $response | jq '.isLast')
+  startAt=$(echo $response | jq '.startAt') # this is zero based
+  isLast=$(echo $response | jq '.isLast') # isLast may not be returned for all operations :|
+
+  # since isLast may not be returned, we compare the nextStartAt with the total
+  total=$(echo $response | jq '.total')
+  nextStartAt=$(( startAt + maxResults ))
 
   echo $(( startAt + maxResults ))
 
@@ -33,7 +40,16 @@ function _jira_get_next_startAt(){
   # to tell calling function to exit the while loop
   # the caling function can detect the exit status by checking
   # $?
-  if [ "$isLast" = "true" ]; then
+  # eg 
+  #   if total of 2 with maxResults of 1, then
+  #     1st call has startAt=0, maxResults=1, total=2, nextStartAt=1 (getting 2nd to 2nd row in next page)
+  #     2nd call has startAt=1, maxResults=1, total=2, nextStartAt=2 - STOP
+  
+  #   if total of 5 with maxResults of 3, then
+  #     1st call has startAt=0, maxResults=3, total=5, nextStartAt=3 (getting 4th to 6th row in next page)
+  #     2nd call has startAt=3, maxResults=3, total=5, nextStartAt=6 - STOP
+  
+  if [ "$isLast" = "true" ] || [[ nextStartAt -ge total ]]; then
     return 1
   else
     return 0
