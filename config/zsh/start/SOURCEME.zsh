dedb68e9f1e182f18ef322d8f41a456670729518
tim goh <timothysl_goh@certisgroup.com>
2022-02-06 16:02:58 +0800

    feat(zsh): migrate to zinit

--- config/zsh/start/SOURCEME.zsh
+++ config/zsh/start/SOURCEME.zsh
@@ -1,6 +1,6 @@
 # source these files that are outside the $XDG_CONFIG_HOME/zsh directory
 # this shortens my zshrc by a few lines
-source $ZSH/oh-my-zsh.sh
+# source $ZSH/oh-my-zsh.sh
 source $XDG_CONFIG_HOME/zsh/p10k/p10k.zsh # p10k configuration
 source $XDG_CONFIG_HOME/lf/icons
 
