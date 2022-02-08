ad7529aedffdf060e6c39e32b2384366943d63a9
tim goh <timothysl_goh@certisgroup.com>
2022-02-08 17:47:59 +0800

    feat(alias): add d and z shortcuts to dirs or config files

--- config/zsh/aliases/goto.zsh
+++ config/zsh/aliases/goto.zsh
@@ -1,26 +1,30 @@
-alias zsht='cd /tmp'
+alias dt='cd /tmp'
+alias dd='cd $HOME/dotfiles'
 
 # zsh boot
-alias zshr='nvim ~/.dotfiles/config/zsh-boot/zshrc'
-alias zshe='nvim ~/.dotfiles/.env'
-alias zshz='nvim ~/.config/zsh/.zshenv'
+alias zr='nvim ~/.dotfiles/config/zsh-boot/zshrc'
+alias ze='nvim ~/.dotfiles/.env'
+alias zz='nvim ~/.config/zsh/.zshenv'
 
 # scratch
 alias aes='nvim ~/certis/docs/wiki/scratch.md'
 alias aesb='bat ~/certis/docs/wiki/scratch.md'
 
 
-alias zshaa='nvim $XDG_CONFIG_HOME/zsh/aliases/goto.zsh'
+alias zaa='nvim $XDG_CONFIG_HOME/zsh/aliases/goto.zsh'
 # config
-alias zshk='nvim ~/.config/kitty/kitty.conf' # the kitty config file
-alias zshgit='nvim ~/.config/zsh/git.zsh'
+alias zk='nvim ~/.config/kitty/kitty.conf'
+alias zgit='nvim ~/.config/zsh/git.zsh'
 alias dos='nvim ~/.dotfiles/install.conf.yaml' # dotbot sourcefile
 alias doi='dotbot --verbose -c ~/.dotfiles/install.conf.yaml'
 
 # directories
+alias d='dirs -v | head -n 10'
 alias -g ...='../..'
 alias -g ....='../../..'
 alias -g .....='../../../..'
 alias -g ......='../../../../..'
 alias -- -='cd -'
 alias la='ls -lAh'
+alias md='mkdir -p'
+alias rd=rmdir
