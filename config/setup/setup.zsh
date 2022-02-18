b7ee6174c03d82e7909edafc760b220582f49620
tim goh <me@timothygoh.org>
2022-02-18 18:24:06 +0800

    feat(setup): add homebrew install

--- config/setup/setup.zsh
+++ config/setup/setup.zsh
@@ -1,3 +1,5 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 git clone https://github.com/tmux-plugins/tpm ~/.local/share/tmux/plugins/tpm
 
