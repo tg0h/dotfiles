c76211e97712e74c3f6f9165e5a96f401f8db128
tim goh <me@timothygoh.org>
2022-02-19 16:21:30 +0800

    setup: add comment

--- config/setup/setup-py.zsh
+++ config/setup/setup-py.zsh
@@ -3,5 +3,6 @@ pyenv global 3.8.12
 
 python3 -m pip install --user pipx
 python3 -m pipx ensurepath
+# pipx installs python applications for you to use. You do not want your applications to depend on your current python development environment version
 pipx install pyfunky
 pipx install urlscan
