13f47b650401791d39f1033811575845ef364dac
tim goh <me@timothygoh.org>
2022-02-20 13:20:02 +0800

    doc(setup): add comment for potential fix for setup-py.zsh

--- config/setup/setup-py.zsh
+++ config/setup/setup-py.zsh
@@ -1,6 +1,7 @@
 pyenv install --skip-existing 3.8.12
 pyenv global 3.8.12
 
+# does this use the system python3 or the pyenv python3 when bootstrapping?
 python3 -m pip install --user pipx
 python3 -m pipx ensurepath
 # pipx installs python applications for you to use. You do not want your applications to depend on your current python development environment version
