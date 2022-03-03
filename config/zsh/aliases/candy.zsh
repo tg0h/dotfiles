fa32b49e2efbebaef879c90c02f6a7614af0cac7
tim goh <me@timothygoh.org>
2022-03-03 12:55:28 +0800

    feat(aliases): show users in cognito dev

--- config/zsh/aliases/candy.zsh
+++ config/zsh/aliases/candy.zsh
@@ -3,7 +3,7 @@ alias clc="~/src/candy/candymold/tools/list-credentials.js"
 alias cgc="~/src/candy/candymold/tools/get-credential.js"
 
 # cognito
-alias cclud="cclu dev | fzf" # list users
+alias cclud="cclu dev | fzf -q 'timothyg'" # list users
 alias cccud="cccu -s dev -c" # create users
 alias cccus="cccu -s staging -c"
 alias ccdud="ccdu -s dev -c" # delete users
