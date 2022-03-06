1ee5377fea4d68bf8b6c15b645a550c791112d5e
tim goh <me@timothygoh.org>
2022-03-06 14:38:01 +0800

    feat(gpg): add gpg encrypt and decrypt

--- config/zsh/aliases/gpg.zsh
+++ config/zsh/aliases/gpg.zsh
@@ -1,12 +1,20 @@
+# encrypt/decrypt
+alias gpe="gpg --symmetric --batch --passphrase-file $GPG_PASS_FILE"
+alias gpd="gpg --decrypt --batch --passphrase-file $GPG_PASS_FILE"
+
 # config
 alias gpc="gpgconf"
-alias gpck="gpgconf --kill gpg-agent"
 alias gpca="gpgconf --list-options gpg-agent"
+
+# gpg agent
+alias gpas="gpgconf --launch gpg-agent" # start agent - normally not needed as gpg starts the agent automatically
+alias gpat="gpg-agent --gpgconf-test" # test gpg agent config
+alias gpak="gpgconf --kill gpg-agent" # kill agent
 
 # list
 alias gpl="gpg --list-keys"
 alias gpls="gpg --list-secret-keys --keyid-format=long"
 
 # export
-alias gpe="gpg --export --armor"
-alias gpes="gpg --export-secret-keys --armor"
+alias gpx="gpg --export --armor"
+alias gpxs="gpg --export-secret-keys --armor"
