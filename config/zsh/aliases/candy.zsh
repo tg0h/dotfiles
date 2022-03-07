596a7e2c9f0063478d1a63afbab347eef784d890
tim goh <me@timothygoh.org>
2022-03-07 19:12:27 +0800

    feat(candy): add description to cognito functions

--- config/zsh/aliases/candy.zsh
+++ config/zsh/aliases/candy.zsh
@@ -4,7 +4,8 @@ alias cgc="~/src/candy/candymold/tools/get-credential.js"
 
 # cognito
 alias cclud="cclu dev | fzf -q 'timothyg'" # list users
-alias cccud="cccu -s dev -c" # create users
-alias cccus="cccu -s staging -c"
-alias ccdud="ccdu -s dev -c" # delete users
-alias ccsud="ccdu -s staging -c"
+alias cclus="cclu staging | fzf -q 'timothyg'" # list users
+alias cccud="cccu -s dev" # create users
+alias cccus="cccu -s staging"
+alias ccdud="ccdu -s dev" # delete users
+alias ccsud="ccdu -s staging"
