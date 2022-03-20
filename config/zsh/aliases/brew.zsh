a36fad8007c6b097849e7113432d088a0197a726
tim goh <me@timothygoh.org>
2022-03-20 10:24:37 +0800

    feat(alias/brew): add bsl

--- config/zsh/aliases/brew.zsh
+++ config/zsh/aliases/brew.zsh
@@ -1,8 +1,10 @@
+# brewfile
 alias bb='brew bundle --file ~/dotfiles/config/homebrew/brewfile'
 alias bbs='nvim ~/dotfiles/config/homebrew/brewfile'
-alias bb='brew bundle --file ~/dotfiles/config/homebrew/brewfile'
 alias bbc='brew bundle --file ~/dotfiles/config/homebrew/brewfile --force cleanup'
 
+# services
 alias bs='brew services'
+alias bsl='brew services list'
 alias bsr='brew services restart'
 alias bss='brew services stop'
