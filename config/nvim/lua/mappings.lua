4e8c3f8ae66a48fac60c4b58a26ca4dd3e4bbdf8
tim goh <me@timothygoh.org>
2022-03-20 12:34:42 +0800

    feat(vim/map): nice remaps

--- config/nvim/lua/mappings.lua
+++ config/nvim/lua/mappings.lua
@@ -9,6 +9,19 @@ vim.g.maplocalleader = ","
 
 map("v", "J", ":m '>+1<CR>gv=gv", default_options)
 map("v", "K", ":m '<-2<CR>gv=gv", default_options)
+-- keep it centred
+map("n", "n", "nzzzv", default_options) -- zv opens folds
+map("n", "N", "Nzzzv", default_options)
+map("n", "J", "mzJ`z", default_options)
+-- granular undo break points
+map("i", ",", ", <C-g>u", default_options)
+map("i", ".", ". <C-g>u", default_options)
+map("i", "!", "! <C-g>u", default_options)
+map("i", "?", "! <C-g>u", default_options)
+-- vimscript . concats strings
+-- m' adds a context mark which also adds to the jump list as a side effect
+map("n", "j", [[ (v:count > 5 ? "m'" . v:count : "") . 'j' ]], expr_options)
+map("n", "k", [[ (v:count > 5 ? "m'" . v:count : "") . 'k' ]], expr_options)
 
 -- Edit file
 map("n", "<localleader>em", ":e $XDG_CONFIG_HOME/nvim/lua/mappings.lua<CR>",
