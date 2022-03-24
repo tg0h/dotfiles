570fcb06e46efcd1dee99ccd2d94d091c206a672
tim goh <me@timothygoh.org>
2022-03-24 09:45:54 +0800

    feat(vim/cmp): use tab to complete

--- config/nvim/lua/setup/cmp.lua
+++ config/nvim/lua/setup/cmp.lua
@@ -76,24 +76,28 @@ cmp.setup({
             behavior = cmp.ConfirmBehavior.Replace,
             select = true -- auto select first entry on enter
         },
-        ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif vim.fn["vsnip#available"](1) == 1 then
-                feedkey("<Plug>(vsnip-expand-or-jump)", "")
-            elseif has_words_before() then
-                cmp.complete()
-            else
-                fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
-            end
-        end, {"i", "s"}),
-        ["<S-Tab>"] = cmp.mapping(function()
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-                feedkey("<Plug>(vsnip-jump-prev)", "")
-            end
-        end, {"i", "s"})
+        ["<Tab>"] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true -- auto select first entry on enter
+        }
+        -- ["<Tab>"] = cmp.mapping(function(fallback)
+        --     if cmp.visible() then
+        --         cmp.select_next_item()
+        --     elseif vim.fn["vsnip#available"](1) == 1 then
+        --         feedkey("<Plug>(vsnip-expand-or-jump)", "")
+        --     elseif has_words_before() then
+        --         cmp.complete()
+        --     else
+        --         fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+        --     end
+        -- end, {"i", "s"}),
+        -- ["<S-Tab>"] = cmp.mapping(function()
+        --     if cmp.visible() then
+        --         cmp.select_prev_item()
+        --     elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+        --         feedkey("<Plug>(vsnip-jump-prev)", "")
+        --     end
+        -- end, {"i", "s"})
     },
 
     sources = {
