d9663a52d50970d7f2e9eedcc927169d653e4843
tim goh <me@timothygoh.org>
2022-03-27 12:27:42 +0800

    feat(vim/mappings): go to buffer

--- config/nvim/lua/mappings.lua
+++ config/nvim/lua/mappings.lua
@@ -108,6 +108,13 @@ map("n", "<A-Down>", ":resize +1<CR>", default_options)
 
 -- Buffers
 map("n", "<A-Esc>", ":e#<CR>", default_options) -- edit previously opened buffer
+
+map("n", "<A-[>", ":BufferLineGoToBuffer 1<CR>", default_options) -- Go to Buffer 1
+map("n", "<A-{>", ":BufferLineGoToBuffer 2<CR>", default_options)
+map("n", "<A-}>", ":BufferLineGoToBuffer 3<CR>", default_options)
+map("n", "<A-(>", ":BufferLineGoToBuffer 4<CR>", default_options)
+map("n", "<A-=>", ":BufferLineGoToBuffer 5<CR>", default_options)
+
 map("n", "<A-g>", ":BufferLineCyclePrev<CR>", default_options) -- Go to left buffer 'tab'
 map("n", "<A-r>", ":BufferLineCycleNext<CR>", default_options) -- Go to right buffer 'tab'
 map("n", "<A-c>", ":Bdelete!<CR>", default_options) -- close buffer
