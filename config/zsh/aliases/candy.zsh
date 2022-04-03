a0ed0e5a1799dfc1c8dd61dc4cdbd769ebe270ab
tim goh <me@timothygoh.org>
2022-04-03 14:36:55 +0800

    feat(alias/candy): convenience aliases for mocking

--- config/zsh/aliases/candy.zsh
+++ config/zsh/aliases/candy.zsh
@@ -9,3 +9,8 @@ alias cccud="cccu -s dev" # create users
 alias cccus="cccu -s staging"
 alias ccdud="ccdu -s dev" # delete users
 alias ccsud="ccdu -s staging"
+
+# mock
+alias mo="bat ./packages/frontend/.env.development*"
+alias mof="echo 'NEXT_PUBLIC_API_MOCKING=false' > ./packages/frontend/.env.development.local"
+alias moe="echo 'NEXT_PUBLIC_API_MOCKING=enabled' > ./packages/frontend/.env.development.local"
