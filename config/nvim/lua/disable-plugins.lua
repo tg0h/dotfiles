f5f852a48965c85cddbdfcca9f76a6ada66069df
tim goh <me@timothygoh.org>
2022-04-14 13:43:23 +0800

    feat(vim/disable-plugins): enable matchit to jump to matching html tag

--- config/nvim/lua/disable-plugins.lua
+++ config/nvim/lua/disable-plugins.lua
@@ -1,24 +1,12 @@
 local disabled_built_ins = {
-    "netrw",
-    "netrwPlugin",
-    "netrwSettings",
-    "netrwFileHandlers",
-    "gzip",
-    "zip",
-    "zipPlugin",
-    "tar",
-    "tarPlugin",
-    "getscript",
-    "getscriptPlugin",
-    "vimball",
-    "vimballPlugin",
-    "2html_plugin",
-    "logipat",
-    "rrhelper",
-    "spellfile_plugin",
-    "matchit"
+    "gzip", "zip", "zipPlugin", "tar", "tarPlugin", "getscript",
+    "getscriptPlugin", "vimball", "vimballPlugin", "2html_plugin", "logipat",
+    "rrhelper", "spellfile_plugin", "netrw", "netrwPlugin", "netrwSettings",
+    "netrwFileHandlers"
+    -- "matchit"
 }
 
+-- matchit is used to jump between matching html tags
 for _, plugin in pairs(disabled_built_ins) do
     vim.g["loaded_" .. plugin] = 1
 end
