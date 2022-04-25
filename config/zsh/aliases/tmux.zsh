21f83224e0dc597099ce7081bb5be519b1cbe1b5
tim goh <me@timothygoh.org>
2022-04-25 09:16:51 +0800

    feat(alias/tmux): display pane, session and window ids

--- config/zsh/aliases/tmux.zsh
+++ config/zsh/aliases/tmux.zsh
@@ -3,6 +3,16 @@ alias uu="tmux ls"
 alias uk="tmux kill-session"
 alias ux="tmux kill-server"
 alias ut="tmux list-commands"
+
+alias us="tmux list-sessions"
+alias uw="tmux list-windows"
+alias up="tmux list-panes"
+alias udp="tmux display -p" # show format variables eg tmux display -p "{#pid}"
+
+alias udpp="tmux display -p #{pane_id}" # show format variables eg tmux display -p "{#pid}"
+alias udps="tmux display -p #{session_id}"
+alias udpw="tmux display -p #{window_id}"
+
 alias uk="tmux list-keys"
 
 # show config
