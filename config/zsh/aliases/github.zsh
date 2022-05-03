26aac91ff4b39bbabd35783f1e53258ef348007d
tim goh <me@timothygoh.org>
2022-05-03 11:59:21 +0800

    feat(alias/github): more repo aliases

--- config/zsh/aliases/github.zsh
+++ config/zsh/aliases/github.zsh
@@ -1,3 +1,8 @@
 alias hl="gh pr list"
 alias hv="gh pr view --web"
 alias ht="gh pr status"
+
+alias hr="gh repo"
+alias hrc="gh repo create --public"
+alias hrl="gh repo list"
+alias hrc="gh repo clone"
