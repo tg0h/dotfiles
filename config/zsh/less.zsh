8848be5bbc31b935e0b9c192630b1c2631cdd86a
tim goh <me@timothygoh.org>
2022-05-03 11:59:02 +0800

    fix(zsh/less): do not show line numbers all the time

--- config/zsh/less.zsh
+++ config/zsh/less.zsh
@@ -1,2 +1,3 @@
 # the less env var specifies the options that should be passed to less when run
-export LESS="--LINE-NUMBERS"
+# but this adds line numbers globally, even eg in git diff
+# export LESS="--LINE-NUMBERS"
