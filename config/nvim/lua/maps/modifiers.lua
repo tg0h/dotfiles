58b65e810902bda6e94ad853823aff06f33c3a85
tim goh <me@timothygoh.org>
2022-05-14 17:50:52 +0800

    feat(vim/maps): c-s-n escapes brackets on same line

--- config/nvim/lua/maps/modifiers.lua
+++ config/nvim/lua/maps/modifiers.lua
@@ -2,6 +2,10 @@ local map = vim.api.nvim_set_keymap
 default_options = {noremap = true, silent = true}
 expr_options = {noremap = true, expr = true, silent = true}
 
+-- go to next ) and then a
+-- map("i", "<C-S-n>", "<Esc>])a", default_options)
+map("i", "<C-S-n>", "<Esc>%%a", default_options)
+
 map("n", "<C-CR>", ":echo 'hello c cr'<CR>", default_options)
 map("n", "<D-CR>", ":echo 'hello D cr'<CR>", default_options)
 map("n", "<C-S-h>", ":echo 'hello csh cr'<CR>", default_options)
