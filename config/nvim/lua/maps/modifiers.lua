acfe4f7c33f3da56b47a3273e8c775b68fe15d44
tim goh <me@timothygoh.org>
2022-05-15 13:17:33 +0800

    docs(vim/maps): comment on tab vs c-i

--- config/nvim/lua/maps/modifiers.lua
+++ config/nvim/lua/maps/modifiers.lua
@@ -3,12 +3,10 @@ default_options = {noremap = true, silent = true}
 expr_options = {noremap = true, expr = true, silent = true}
 
 -- go to next ) and then a
--- map("i", "<C-S-n>", "<Esc>])a", default_options)
 map("i", "<C-S-n>", "<Esc>%%a", default_options)
 
-map("n", "<C-CR>", ":echo 'hello c cr'<CR>", default_options)
-map("n", "<D-CR>", ":echo 'hello D cr'<CR>", default_options)
-map("n", "<C-S-h>", ":echo 'hello csh cr'<CR>", default_options)
-map("n", "<C-m>", ":echo 'test m'<CR>", default_options)
+-- only works in kitty without tmux and using autocmd to send csi u
+-- map("i", "<tab>", "a", default_options)
+-- map("i", "<C-m>", "m", default_options)
 
 map("n", "<F5>", ":luafile %<CR>", default_options)
