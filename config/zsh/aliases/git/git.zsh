e2cff8fe1c396e680dcd1c98c14c60983c7d68d6
tim goh <me@timothygoh.org>
2022-05-16 09:01:47 +0800

    feat(git/commit): gg -c - specify the checkpoint message

--- config/zsh/aliases/git/git.zsh
+++ config/zsh/aliases/git/git.zsh
@@ -6,7 +6,8 @@ alias g..="gloz -20"
 alias g,.="glodm"
 
 alias ggp="gg && gp"
-alias gge="gg -e"
+alias gge="gg -e" # allow empty commit
+alias ggc="gg -c" # specify a checkpoint message
 alias gga="git add --all && gcan!" # add and amend previous commit
 
 alias ge="git clone"
