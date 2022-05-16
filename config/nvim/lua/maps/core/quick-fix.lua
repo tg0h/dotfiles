58262cda6b92a844eadc37c54ce06b7ddc96e20b
tim goh <me@timothygoh.org>
2022-05-16 10:47:17 +0800

    docs(vim/maps): point out use of custom function

--- config/nvim/lua/maps/core/quick-fix.lua
+++ config/nvim/lua/maps/core/quick-fix.lua
@@ -2,6 +2,7 @@ local map = vim.api.nvim_set_keymap
 default_options = {noremap = true, silent = true}
 expr_options = {noremap = true, expr = true, silent = true}
 
+-- my own vim function!
 map("n", "<C-q>", ":lua require('tg.quickfix').ToggleQFList(1)<CR>",
     default_options)
 map("n", "<C-j>", ":cnext<CR>", default_options)
