d0a90af5b6a9b58b66c60f0e3db6d8514705e995
tim goh <me@timothygoh.org>
2022-05-17 12:24:31 +0800

    feat(hammerspoon): add hammerspoon

---
+++ config/hammerspoon/init.lua
@@ -0,0 +1,10 @@
+-- https://github.com/Hammerspoon/hammerspoon/issues/2175
+-- defaults write org.hammerspoon.Hammerspoon MJConfigFile "~/.config/hammerspoon/init.lua"
+
+-- hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
+--   hs.alert.show("Hello World!")
+-- end)
+
+-- https://github.com/koekeishiya/yabai/discussions/1011
+-- TODO: use hammerspoon to run a yabai command
+
