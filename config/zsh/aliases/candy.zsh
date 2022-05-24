8e3133eb9e1fe473248b4722cb4a977af2498035
tim goh <me@timothygoh.org>
2022-05-24 13:34:20 +0800

    feat(alias/candy): print env info

--- config/zsh/aliases/candy.zsh
+++ config/zsh/aliases/candy.zsh
@@ -6,11 +6,12 @@ alias cgc="~/src/candy/old/candymold/tools/get-credential.js"
 alias cclud="cclu dev | fzf -q 'timothyg'" # list users
 alias cclus="cclu staging | fzf -q 'timothyg'" # list users
 alias cccud="cccu -s dev" # create users
-alias cccus="cccu -s staging"
+alias cccus="cccu -s staging -c"
+
 alias ccdud="ccdu -s dev" # delete users
 alias ccsud="ccdu -s staging"
 
 # mock
-alias mo="bat ./packages/frontend/.env.development*"
+alias mo="bat ./packages/frontend/.env.development* ./packages/api/api/.env.development*"
 alias mof="echo 'NEXT_PUBLIC_API_MOCKING=false' > ./packages/frontend/.env.development.local"
 alias moe="echo 'NEXT_PUBLIC_API_MOCKING=enabled' > ./packages/frontend/.env.development.local"
