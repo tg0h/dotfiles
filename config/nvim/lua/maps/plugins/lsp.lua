4a23c8a0db3ed031703e12b649a29cefb99d7eaa
tim goh <me@timothygoh.org>
2022-05-29 21:42:03 +0800

    feat(vim/maps): map a-u to lsp references

--- config/nvim/lua/maps/plugins/lsp.lua
+++ config/nvim/lua/maps/plugins/lsp.lua
@@ -3,6 +3,6 @@ default_options = {noremap = true, silent = true}
 expr_options = {noremap = true, expr = true, silent = true}
 
 map("n", "<A-a>", ":lua vim.lsp.buf.definition()<CR>", default_options)
+map("n", "<A-u>", ":lua vim.lsp.buf.references()<CR>", default_options) -- prev diagnostic
 -- map("n", "<S-A-c>", ":lua vim.diagnostic.goto_next()<CR>", default_options) -- next diagnostic
 -- map("n", "<S-A-r>", ":lua vim.diagnostic.goto_prev()<CR>", default_options) -- prev diagnostic
-map("n", "<A-i>", ":lua vim.lsp.buf.references()<CR>", default_options) -- prev diagnostic
