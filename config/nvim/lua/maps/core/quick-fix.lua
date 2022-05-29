659480d197638d33e9b3c8d182c29f252af4fd6a
tim goh <me@timothygoh.org>
2022-05-29 21:42:45 +0800

    feat(vim/maps): map alt q,j,k to qf list

--- config/nvim/lua/maps/core/quick-fix.lua
+++ config/nvim/lua/maps/core/quick-fix.lua
@@ -3,7 +3,10 @@ default_options = {noremap = true, silent = true}
 expr_options = {noremap = true, expr = true, silent = true}
 
 -- my own vim function!
-map("n", "<C-q>", ":lua require('tg.quickfix').ToggleQFList(1)<CR>",
+-- quicker shortcuts since lsp goto def is called with A-i
+map("n", "<A-q>", ":lua require('tg.quickfix').ToggleQFList(1)<CR>",
     default_options)
-map("n", "<C-j>", ":cnext<CR>", default_options)
-map("n", "<C-k>", ":cprev<CR>", default_options)
+-- map("n", "<C-j>", ":cnext<CR>", default_options)
+-- map("n", "<C-k>", ":cprev<CR>", default_options)
+map("n", "<A-j>", ":cnext<CR>", default_options)
+map("n", "<A-k>", ":cprev<CR>", default_options)
