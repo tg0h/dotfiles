a8aab7180b1d2f3360ccd6187646bf0b5b397487
tim goh <timslgoh@gmail.com>
2022-07-10 20:46:58 +0800

    feat(vim/git-blame): move author to beginning of git blame

--- config/nvim/lua/setup/git-blame.lua
+++ config/nvim/lua/setup/git-blame.lua
@@ -1,6 +1,6 @@
 local g = vim.g
 g.gitblame_enabled = 0
 
-g.gitblame_message_template = '<summary> • <date> • <author> • <sha>'
+g.gitblame_message_template = '<author> • <summary> • <date> • <sha>'
 
 g.gitblame_date_format = '%r - %a %d %b %y %X'
