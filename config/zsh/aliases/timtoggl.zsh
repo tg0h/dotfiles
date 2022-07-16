f518c67a5f498b2454fbff874cfc8ae9e327555f
tim goh <timslgoh@gmail.com>
2022-07-16 14:32:06 +0800

    feat(alias/timtoggl): add certis aliases

--- config/zsh/aliases/timtoggl.zsh
+++ config/zsh/aliases/timtoggl.zsh
@@ -61,3 +61,18 @@ alias ttsm="timt -w s -t s -s 1" #since start of this month
 alias ttsd="timt -w s -t s -d"
 alias ttsdt="timt -w s -t s -d -s today"
 alias ttsdm="timt -w s -t s -d -r 0m"
+
+## certis - m
+alias tmd="timt -w m -t d" # client, project, tag
+alias tmdt="timt -w m -t d -s today"
+alias tmdd="timt -w m -t d --dd" # timeline report
+alias tmw="timt -w m -t w"
+alias tmww="timt -w m -t d --ww -s 0cw" #detailed report starting this week
+alias tmm="timt -w m -t d --ww -s 1" #wide report starting from the 1st
+alias tms="timt -w m -t s"
+alias tmst="timt -w m -t s -s today"
+alias tmsw="timt -w m -t s -s 0cw" #since start of this week
+alias tmsm="timt -w m -t s -s 1" #since start of this month
+alias tmsd="timt -w m -t s -d"
+alias tmsdt="timt -w m -t s -d -s today"
+alias tmsdm="timt -w m -t s -d -r 0m"
