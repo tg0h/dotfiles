81faf82ef0639d914bce715c62b3a0137a0548a5
tim goh <timslgoh@gmail.com>
2022-07-24 19:50:12 +0800

    feat(alias/docker): more aliases

--- config/zsh/aliases/docker.zsh
+++ config/zsh/aliases/docker.zsh
@@ -1,3 +1,14 @@
+################################################### Compose
+alias dk='docker compose'
+alias dku='docker compose up'
+alias dkd='docker compose down'
+alias dkud='docker compose up -d'
+alias dkl='docker compose ls'
+alias dkla='docker compose ls -a'
+alias dkp='docker compose ps'
+alias dkg='docker compose logs'
+alias dkgf='docker compose logs --follow'
+
 ################################################### Containers
 alias dc='docker container'
 
@@ -26,11 +37,13 @@ alias dcst='docker container stats'
 ## LIST
 # alias dcl='docker container ls'
 alias dcll="docker container ls | rg -v 'dkr.ecr.ap-southeast-1.amazonaws.com' | rg -v 'composer'"
-alias dcla='dcl -a -f' # exclude candy containers
+alias dcla='dcl -a'
+alias dclaf='dcl -af' # exclude candy containers
 alias dclaa="docker container ls -a | rg -v 'dkr.ecr.ap-southeast-1.amazonaws.com' | rg -v 'composer'"
 
 ## REMOVE
 alias dcm='docker container rm '
+alias dcmf='docker container rm -f'
 alias dcma='docker container rm $(docker container ls -aq)'
 
 ## RUN
@@ -41,6 +54,7 @@ alias dcrdn='docker container run -d --name'
 
 ## START
 alias dcs='docker container start'
+alias dcsa='docker container start --attach --interactive'
 
 
 ## STOP
@@ -49,14 +63,27 @@ alias dcp='docker container stop'
 
 ################################################### Images
 alias di='docker image'
+alias dih='docker image history'
+alias dii='docker image inspect'
+
+alias dit='docker image tag'
+alias dip='docker image push'
 
 alias dir='docker image rm'
+
+alias dib='docker image build'
 
 alias dil='docker image ls'
 alias dill="docker image ls | rg -v '^2.*'"
 
 ################################################### Network
 alias dn='docker network'
+alias dnc='docker network create'
+alias dnr='docker network rm'
+alias dno='docker network connect'
+alias dnd='docker network disconnect'
+alias dnl='docker network ls'
+alias dni='docker network inspect'
 
 ################################################### Volume
 alias dv='docker volume'
