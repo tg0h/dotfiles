d4a574607893ca4ec6cb9b01703933c3d1eb3290
tim goh <timslgoh@gmail.com>
2022-08-06 16:02:41 +0800

    feat(zsh/bindkey): add new keywidgets (3h)

--- config/zsh/bindkey.zsh
+++ config/zsh/bindkey.zsh
@@ -4,10 +4,20 @@
 bindkey -r "^G" # remove ctrl g so that junegunn's fzf_git functions work
 # bindkey -r "^N"
 # bindkey -r "^K"
-bindkey -r "^H"
-bindkey -r "^S"
+# bindkey -r "^H"
+# bindkey -r "^S"
+# bindkey -r "^[S"
 
+# unbind Meh-_ from copy-prev-word and map to copy-prev-shell-word instead
+bindkey '^[^_' copy-prev-shell-word
+bindkey '^[^h' run-help
+bindkey '^[^r' redo
 
+# alt-ctrl-t to transpose words
+bindkey '^[^t' transpose-words
+
+# replace current region with contents of kill buffer
+bindkey '^[v' put-replace-selection
 # https://unix.stackexchange.com/questions/51933/zsh-copy-and-paste-like-emacs
 # alt-w
 zle -N pb-copy-region-as-kill
@@ -27,19 +37,20 @@ zle     -N   fzf-search-bookmarks-widget
 zle -N _fzf-redraw-prompt # used by fzf bookmarks widget to redraw prompt
 
 zle -N fzf-docker-widget
-bindkey '^B' fzf-docker-widget
+# replaces the quoted-insert widget
+bindkey '^V' fzf-docker-widget
 
 # npm
-zle -N fzf-search-package-widget
-bindkey '^s^g' fzf-search-package-widget
-zle -N fzf-search-package-downwards-widget
-bindkey '^s^f' fzf-search-package-downwards-widget
+# zle -N fzf-search-package-widget
+# bindkey '^s^g' fzf-search-package-widget
+# zle -N fzf-search-package-downwards-widget
+# bindkey '^s^f' fzf-search-package-downwards-widget
 
 zle -N fzf-search-filenameAndContent-widget
-bindkey '^h' fzf-search-filenameAndContent-widget
+bindkey '^[-' fzf-search-filenameAndContent-widget
 
 zle -N fzf-search-cloudwatch-logs-widget
-bindkey '^s^t' fzf-search-cloudwatch-logs-widget
+bindkey '^[^r' fzf-search-cloudwatch-logs-widget
 
 # fzf git widgets
 zle -N fzf-gh-widget # commits
