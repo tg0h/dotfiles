a371aa2c0880c743c564fa7360197c951e6dcb6e
tim goh <timslgoh@gmail.com>
2022-09-19 08:49:20 +0800

    feat(alias/github): more aliases

--- config/zsh/aliases/github.zsh
+++ config/zsh/aliases/github.zsh
@@ -1,8 +1,15 @@
+# pr
+alias hc="gh pr create"
+alias hc.="gh pr create --fill" # pull request - create pr with defaults
 alias hl="gh pr list"
-alias hv="gh pr view --web"
+alias hv="gh pr view --web" # pull request - view pr after creating it
 alias ht="gh pr status"
 
+# repo
 alias hr="gh repo"
-alias hrc="gh repo create --public"
+alias hrc="gh repo create"
+alias hrcp="gh repo create --public"
+alias hrcpp="gh repo create --private"
 alias hrl="gh repo list"
-alias hrc="gh repo clone"
+alias hro="gh repo clone"
+alias hrd="gh repo delete"
