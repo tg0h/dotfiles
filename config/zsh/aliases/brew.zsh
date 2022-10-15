ac8b7c6dba8fc9f36b126f186c262719408238c3
tim goh <timslgoh@gmail.com>
2022-10-15 13:59:52 +0800

    feat(alias/brew): bu to uninstall

--- config/zsh/aliases/brew.zsh
+++ config/zsh/aliases/brew.zsh
@@ -3,6 +3,9 @@ alias bb='brew bundle --file ~/dotfiles/config/homebrew/brewfile'
 alias bbs='nvim ~/dotfiles/config/homebrew/brewfile'
 alias bbc='brew bundle --file ~/dotfiles/config/homebrew/brewfile --force cleanup'
 
+# uninstall
+alias bu='brew uninstall'
+
 # services
 alias bs='brew services'
 alias bsl='brew services list'
