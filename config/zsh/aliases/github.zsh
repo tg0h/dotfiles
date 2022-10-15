5fe4629bbfbbb420043378df678b9241ef1a79a3
tim goh <timslgoh@gmail.com>
2022-10-15 15:26:23 +0800

    feat(alias/github): hrc clones repo

--- config/zsh/aliases/github.zsh
+++ config/zsh/aliases/github.zsh
@@ -1,17 +1,20 @@
 # pr
-alias hc.="hc -f" # create pr but prefill title and body
-alias hc,="hc -t " # create pr with title
 alias hl="gh pr list"
 alias hv="gh pr view --web" # pull request - view pr after creating it
+alias hc.="hc -f" # create pr but prefill title and body
+alias hc,="hc -t " # create pr with title
 alias ht="gh pr status"
 
 # repo
 alias hr="gh repo"
+alias hrl="gh repo list"
 alias hr.="gh repo view --web"
-
-alias hrc="gh repo create"
+## create
 alias hrcp="gh repo create --public"
 alias hrcpp="gh repo create --private"
-alias hrl="gh repo list"
-alias hro="gh repo clone"
+## clone
+alias hrc="gh repo clone"
+## delete
 alias hrd="gh repo delete"
+alias hrdf="gh repo delete --confirm"
+
