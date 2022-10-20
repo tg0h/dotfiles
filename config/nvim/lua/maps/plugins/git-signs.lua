68c69d2eb4e8ffa38e052d34a50567cd1a220851
tim goh <timslgoh@gmail.com>
2022-10-21 07:28:01 +0800

    feat(vim/maps): S-A-g/r to move to prev/next hunk

--- config/nvim/lua/maps/plugins/git-signs.lua
+++ config/nvim/lua/maps/plugins/git-signs.lua
@@ -2,5 +2,5 @@ local map = vim.api.nvim_set_keymap
 default_options = {noremap = true, silent = true}
 expr_options = {noremap = true, expr = true, silent = true}
 
-map("n", "<S-A-c>", ":lua require('gitsigns').next_hunk()<CR>", default_options) -- next git hunk
-map("n", "<S-A-r>", ":lua require('gitsigns').prev_hunk()<CR>", default_options) -- prev git hunk
+map("n", "<S-A-g>", ":lua require('gitsigns').prev_hunk()<CR>", default_options) -- prev git hunk
+map("n", "<S-A-r>", ":lua require('gitsigns').next_hunk()<CR>", default_options) -- next git hunk
