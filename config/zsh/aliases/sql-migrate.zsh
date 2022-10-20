0d3caf5ae2122aaba6d4d6c8444a3a76787e740d
tim goh <timslgoh@gmail.com>
2022-10-21 07:24:28 +0800

    feat(alias/sql-migrate): add sql migrate aliases

---
+++ config/zsh/aliases/sql-migrate.zsh
@@ -0,0 +1,10 @@
+alias sms='sql-migrate status -config=dbconfig-local.yml'
+
+alias smr='sql-migrate redo -config=dbconfig-local.yml'
+alias smrd='sql-migrate redo -dryrun -config=dbconfig-local.yml'
+
+alias smu='sql-migrate up -config=dbconfig-local.yml'
+alias smud='sql-migrate up -dryrun -config=dbconfig-local.yml'
+
+alias smd='sql-migrate down -config=dbconfig-local.yml'
+alias smdd='sql-migrate down -dryrun -config=dbconfig-local.yml'
