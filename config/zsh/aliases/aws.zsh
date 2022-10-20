49b2628841d446275d56f6d258fa4dcc61203925
tim goh <timslgoh@gmail.com>
2022-10-21 07:25:50 +0800

    feat(alias/aws): switch profiles

--- config/zsh/aliases/aws.zsh
+++ config/zsh/aliases/aws.zsh
@@ -1,4 +1,6 @@
 # aws profile switcher
+alias asdt='asd t' # default to aws tim profile
+alias asdc='asd c' # default to aws candy profile
 alias ast='as t' # switch to aws profile tim
 alias asc='as c' # switch to aws candy
 
@@ -13,4 +15,5 @@ alias aqsl="aqs -l"
 # cdk
 alias cd.="cdk synth"
 alias cd,="cdk ls"
+alias cdi="cdk diff"
 alias cdp="cdk deploy"
