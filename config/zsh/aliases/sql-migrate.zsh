220adcb5843e65c3ffd412440e5eb3895e8e0934
tim goh <timslgoh@gmail.com>
2022-10-26 06:10:41 +0800

    feat(alias/sql-migrate): add -limit to down

--- config/zsh/aliases/sql-migrate.zsh
+++ config/zsh/aliases/sql-migrate.zsh
@@ -1,3 +1,5 @@
+#TODO: ae does not work because sql-migrate filename has a dash in it ?
+
 alias sms='sql-migrate status -config=dbconfig-local.yml'
 
 alias smr='sql-migrate redo -config=dbconfig-local.yml'
@@ -6,5 +8,5 @@ alias smrd='sql-migrate redo -dryrun -config=dbconfig-local.yml'
 alias smu='sql-migrate up -config=dbconfig-local.yml'
 alias smud='sql-migrate up -dryrun -config=dbconfig-local.yml'
 
-alias smd='sql-migrate down -config=dbconfig-local.yml'
-alias smdd='sql-migrate down -dryrun -config=dbconfig-local.yml'
+alias smd='sql-migrate down -config=dbconfig-local.yml -limit'
+alias smdd='sql-migrate down -dryrun -config=dbconfig-local.yml -limit'
