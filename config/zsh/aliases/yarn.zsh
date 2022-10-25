06759a9b23c2e64bcaf5b1fa1fbd202d3298d5f7
tim goh <timslgoh@gmail.com>
2022-10-25 09:53:30 +0800

    feat(alias/yarn): more yarn aliases

--- config/zsh/aliases/yarn.zsh
+++ config/zsh/aliases/yarn.zsh
@@ -1,10 +1,20 @@
+alias yi="yarn install"
+
+alias yy="yarn why"
+
+alias yc="yarn config"
+alias ycg="yarn config get"
+alias ycs="yarn config set"
+
 alias ys="yarn start"
 alias ysf="yarn start-frontend"
 alias ysa="yarn start-api"
 alias ysd="yarn start-daemon"
 
-alias yw="yarn workspaces"
-alias ywl="yarn workspaces list"
+# alias yw="yarn workspaces"
+alias yw="yarn workspaces list"
 
-alias yp="yarn workspace @rc-main/ui-lib storybook"
+# alias yp="yarn workspace @rc-main/ui-lib storybook"
 alias yb="yarn workspaces foreach -v run build"
+
+alias yp="yarn plugin list"
