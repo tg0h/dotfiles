106e745aee69abfa617efd14d1ebdecfe3452087
tim goh <timslgoh@gmail.com>
2022-10-30 07:57:26 +0800

    refactor(alias/mycli): rename d. to m.

--- config/zsh/aliases/mycli.zsh
+++ config/zsh/aliases/mycli.zsh
@@ -1,4 +1,4 @@
-alias d.='mycli -d local' # connect to candy db local
-alias d..='mycli -d prod' # connect to candy db prod
-alias d,='mycli -d stg' # connect to candy db staging
+alias m.='mycli -d local' # connect to candy db local
+alias m..='mycli -d prod' # connect to candy db prod
+alias m,='mycli -d stg' # connect to candy db staging
 # alias dx='mycli -d pentel_dsn -e'
