1289960800db37457188cedb934ca8847bce14c6
tim goh <timslgoh@gmail.com>
2022-12-12 22:02:38 +0800

    refactor(alias/mycli): change profile name to staging

--- config/zsh/aliases/mycli.zsh
+++ config/zsh/aliases/mycli.zsh
@@ -1,4 +1,4 @@
 alias m.='mycli -d local' # connect to candy db local
 alias m..='mycli -d prod' # connect to candy db prod
-alias m,='mycli -d stg' # connect to candy db staging
+alias m,='mycli -d staging' # connect to candy db staging
 # alias dx='mycli -d pentel_dsn -e'
