a12dc9da61b57f4d1469ea13fe9e8b6eed9abe56
tim goh <timslgoh@gmail.com>
2022-12-12 22:03:38 +0800

    feat(vim/lsp): add sql lang server

--- config/nvim/lua/setup/lsp.lua
+++ config/nvim/lua/setup/lsp.lua
@@ -13,7 +13,7 @@ capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
 
 local servers = {
     "tsserver", "jsonls", "dockerls", "yamlls", "sumneko_lua",
-    "solargraph", "pyright"
+    "solargraph", "pyright", "sqlls"
 }
 
 -- Use a loop to conveniently call 'setup' on multiple servers
