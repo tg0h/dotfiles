fb5cf7c65e9d0334ef7ec75a20f8ff93720dff81
tim goh <timslgoh@gmail.com>
2022-12-18 08:18:22 +0800

    feat(aliases): add suffix aliases

---
+++ config/zsh/aliases/suffix/suffix.zsh
@@ -0,0 +1,13 @@
+# open files with editor
+# eg test.txt enter becomes nvim test.txt
+alias -s txt=nvim
+alias -s json=nvim
+# alias -s zsh=nvim # interferes with alias zsh.h.zsh which runs the script
+# alias -s js=nvim
+# alias -s ts=nvim
+alias -s yaml=nvim
+alias -s yml=nvim
+alias -s config=nvim
+alias -s cfg=nvim
+alias -s conf=nvim
+alias -s skdhrc=nvim
