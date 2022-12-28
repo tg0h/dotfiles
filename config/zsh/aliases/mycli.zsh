e3b9db8c693ef67363cb3263ebc54ec4ad3e0167
tim goh <timslgoh@gmail.com>
2022-12-28 10:45:01 +0800

    feat(alias/mycli): add customer tracks

--- config/zsh/aliases/mycli.zsh
+++ config/zsh/aliases/mycli.zsh
@@ -3,4 +3,6 @@ alias m.='mycli -d local' # connect to candy db local
 alias m,='mycli -d staging' # connect to candy db staging
 alias m,.='mycli -d prod-read' # connect to candy db prod-read
 alias m..='mycli -d prod' # connect to candy db prod
+
+alias mo='mycli -d prod-customer-tracks' # connect to customer tracks
 # alias dx='mycli -d pentel_dsn -e'
