4d55083cccee1f843e2aa573087684c570e22aeb
tim goh <timslgoh@gmail.com>
2022-12-31 11:58:55 +0800

    refactor(lib/entr): supply dir as first arg
    remove -d option for faster use of nt

--- config/zsh/aliases/nt.zsh
+++ config/zsh/aliases/nt.zsh
@@ -1,2 +1,3 @@
+alias nt.="nt '$HOME/dotfiles $HOME/.local/bin/scratch'"
 alias nt,="nt -d $CANDY_PROJ/packages/common-services"
-alias nt.="nt -d $CANDY_PROJ/packages/api/api"
+# alias nt.="nt -d $CANDY_PROJ/packages/api/api"
