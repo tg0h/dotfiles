b60024b77db7a3d418b9ddc6ab750289ac284a34
tim goh <timslgoh@gmail.com>
2023-01-29 12:39:06 +0800

    feat(kitty): ctrl-alt t,h finger roll to save and run
    save nvim in current window, run previous command in other kitty window

--- config/nvim/lua/maps/defaults.lua
+++ config/nvim/lua/maps/defaults.lua
@@ -30,7 +30,11 @@ map("n", "k", [[ (v:count > 5 ? "m'" . v:count : "") . 'k' ]], expr_options)
 -- map("n", "gx", ":!open <C-R><C-A><CR>", expr_options)
 -- https://vi.stackexchange.com/questions/27255/how-can-i-make-a-single-mapping-take-effect-for-both-normal-mode-insert-mode-a (you can't)
 -- define for both normal and insert modes
-map("n", "<M-C-z>", ":cquit<CR>", default_options) -- exit with error code
-map("i", "<M-C-z>", "<Esc>:cquit<CR>", default_options) -- exit with error code
+map("n", "<C-M-z>", ":cquit<CR>", default_options) -- exit with error code
+map("i", "<C-M-z>", "<Esc>:cquit<CR>", default_options) -- exit with error code
 
-map("i", "<M-C-S>", "<Esc>:wq<CR>", default_options) -- insert mode - save and exit
+map("n", "<C-M-t>", ":w<CR>", default_options) -- normal mode save
+map("i", "<C-M-t>", "<Esc>:w<CR>a", default_options) -- insert mode save
+
+map("i", "<C-M-S>", "<Esc>:wq<CR>", default_options) -- insert mode - save and exit
+
