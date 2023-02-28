d36469bef1f4efcac26c098b85f79ea67537339b
tim goh <timslgoh@gmail.com>
2023-02-28 16:23:27 +0800

    feat(alias/mycli): alias to connect to customerTracks db

--- config/zsh/aliases/mycli.zsh
+++ config/zsh/aliases/mycli.zsh
@@ -4,5 +4,6 @@ alias m,='mycli -d staging' # connect to candy db staging
 alias m,.='mycli -d prod-read' # connect to candy db prod-read
 alias m..='mycli -d prod' # connect to candy db prod
 
-alias mo='mycli -d prod-customer-tracks' # connect to customer tracks
+alias mo='mycli -d staging-customer-tracks' # connect to customer tracks
+alias moe='mycli -d prod-customer-tracks' # connect to customer tracks
 # alias dx='mycli -d pentel_dsn -e'
