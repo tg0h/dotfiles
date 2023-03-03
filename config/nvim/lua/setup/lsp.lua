f2250c868007770091f3e0d6a5d0dc6419a9d591
tim goh <timslgoh@gmail.com>
2023-03-03 08:48:55 +0800

    fix(vim/lsp): disable annoying lua lsp prompt
    disable lua lsp prompt 'do you need to configure your work enviroment as
    ...'
    https://github.com/neovim/nvim-lspconfig/issues/1700

--- config/nvim/lua/setup/lsp.lua
+++ config/nvim/lua/setup/lsp.lua
@@ -61,6 +61,7 @@ for _, lsp in ipairs(servers) do
               workspace = {
                 -- Make the server aware of Neovim runtime files
                 library = vim.api.nvim_get_runtime_file("", true),
+                checkThirdParty = false, -- disable annoying 'do you need to configure your work enviroment as ... prompt'
               },
               -- Do not send telemetry data containing a randomized but unique identifier
               telemetry = {
