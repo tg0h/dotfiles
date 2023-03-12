ca4f40ba22498566b67d3e71f883fc0aa3bbe64d
tim goh <timslgoh@gmail.com>
2023-03-12 11:12:10 +0800

    feat(vim/formatter): use luaformatter config file
    - do not supply args
    - use config.yaml in $XDG_CONFIG_HOME/luaformatter

--- config/nvim/lua/setup/formatter.lua
+++ config/nvim/lua/setup/formatter.lua
@@ -7,36 +7,17 @@ augroup END
 
 local function prettierFormat()
     return {
-        exe = "prettier",
-        args = {
-            "--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0))
-        },
+        exe = 'prettier',
+        args = {'--stdin-filepath', vim.fn.fnameescape(vim.api.nvim_buf_get_name(0))},
         stdin = true
     }
 end
 
-require("formatter").setup({
+require('formatter').setup({
     filetype = {
         -- install luarocks with brew
         -- https://github.com/Koihik/LuaFormatter
-        lua = {
-            function()
-                return {
-                    exe = "lua-format",
-                    args = {
-                        "-i", "--no-keep-simple-function-one-line",
-                        "--no-break-after-operator", "--break-after-table-lb",
-                        "--no-break-before-functioncall-rp",
-                        "--no-chop-down-parameter",
-                        "--single-quote-to-double-quote",
-                        "--no-keep-simple-control-block-one-line",
-                        "--no-keep-simple-function-one-line",
-                        "--no-break-after-functioncall-lp"
-                    },
-                    stdin = true
-                }
-            end
-        },
+        lua = {function() return {exe = 'lua-format', args = {'-i'}, stdin = true} end},
 
         html = {prettierFormat},
         typescript = {prettierFormat},
