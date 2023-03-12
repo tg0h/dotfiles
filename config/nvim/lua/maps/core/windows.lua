2d74f7fdfb86bab6c6ed163b45050d6e97c6d8bd
tim goh <timslgoh@gmail.com>
2023-03-12 10:22:50 +0800

    feat(vim/tg/toggle-window-zoom): S-A-z toggles window zoom

--- config/nvim/lua/maps/core/windows.lua
+++ config/nvim/lua/maps/core/windows.lua
@@ -9,6 +9,8 @@ map("n", "<S-A-h>", ":wincmd s<CR>", default_options) -- horizontal split
 map("n", "<S-A-t>", ":wincmd v<CR>", default_options) -- vertical split
 map("n", "<S-A-z>", ":wincmd _<CR>:wincmd |<CR>", default_options) -- zoom
 map("n", "<S-A-e>", ":wincmd =<CR>", default_options) -- equal splits
+-- map("n", "<S-A-z>", ":wincmd _<CR>:wincmd |<CR>", default_options) -- zoom
+map("n", "<S-A-z>", ":lua require'tg.toggle-window-zoom'.toggle()<CR>", default_options) -- zoom
 map("n", "<S-A-e>", ":wincmd =<CR>", default_options) -- equal splits
 map("n", "+", ":res +5<CR>", default_options) -- equal splits
 map("n", "-", ":res -5<CR>", default_options) -- equal splits
---
+++ config/nvim/lua/tg/toggle-window-zoom.lua
@@ -0,0 +1,19 @@
+local g = vim.g
+local cmd = vim.api.nvim_command
+
+g.tg_toggle_window_zoom = 0
+
+local M = {}
+
+function M.toggle()
+    if vim.g.tg_toggle_window_zoom == 0 then
+        g.tg_toggle_window_zoom = 1
+        cmd "wincmd _"
+        cmd "wincmd |"
+    else
+        g.tg_toggle_window_zoom = 0
+        cmd "wincmd ="
+    end
+end
+
+return M
