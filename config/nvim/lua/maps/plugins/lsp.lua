b6380aeb849a38a276ffd16266d2a7968371ca7a
tim goh <timslgoh@gmail.com>
2023-03-12 10:35:33 +0800

    feat(vim/maps): A-l opens lsp signature help

--- config/nvim/lua/maps/plugins/lsp.lua
+++ config/nvim/lua/maps/plugins/lsp.lua
@@ -2,7 +2,6 @@ local map = vim.api.nvim_set_keymap
 default_options = {noremap = true, silent = true}
 expr_options = {noremap = true, expr = true, silent = true}
 
-
 map("n", "<A-e>", ":lua vim.lsp.buf.rename()<CR>", default_options) -- rename
 
 map("n", "<A-a>", ":lua vim.lsp.buf.definition()<CR>", default_options) -- go to def
@@ -12,3 +11,5 @@ map("n", "<C-A-g>", ":lua vim.diagnostic.goto_prev()<CR>", default_options) -- p
 map("n", "<C-A-r>", ":lua vim.diagnostic.goto_next()<CR>", default_options) -- next diagnostic
 
 map("n", "<C-A-l>", ":lua vim.diagnostic.open_float()<CR>", default_options) -- show diagnostic
+
+map("n", "<A-l>", ":lua vim.lsp.buf.signature_help()<CR>", default_options) -- signature help
