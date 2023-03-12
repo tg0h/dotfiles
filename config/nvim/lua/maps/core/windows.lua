caf9f85f6c500755c30f08ceb1557d51daeb1f77
tim goh <timslgoh@gmail.com>
2023-03-12 10:31:58 +0800

    feat(vim/maps): standardise window commands
    - n - vertical
    - b - horizontal
    - z - zoom (toggle)

--- config/nvim/lua/maps/core/windows.lua
+++ config/nvim/lua/maps/core/windows.lua
@@ -4,14 +4,15 @@ expr_options = {noremap = true, expr = true, silent = true}
 
 -- window movement
 map("n", "<A-w>", ":wincmd q<CR>", default_options) -- close window
-map("n", "<S-A-d>", ":wincmd o<CR>", default_options) -- close all other splits
-map("n", "<S-A-h>", ":wincmd s<CR>", default_options) -- horizontal split
-map("n", "<S-A-t>", ":wincmd v<CR>", default_options) -- vertical split
-map("n", "<S-A-z>", ":wincmd _<CR>:wincmd |<CR>", default_options) -- zoom
-map("n", "<S-A-e>", ":wincmd =<CR>", default_options) -- equal splits
+
+map("n", "<S-A-c>", ":wincmd o<CR>", default_options) -- close all other splits
+
+map("n", "<S-A-n>", ":wincmd v<CR>", default_options) -- vertical split
+map("n", "<S-A-b>", ":wincmd s<CR>", default_options) -- horizontal split
+
 -- map("n", "<S-A-z>", ":wincmd _<CR>:wincmd |<CR>", default_options) -- zoom
 map("n", "<S-A-z>", ":lua require'tg.toggle-window-zoom'.toggle()<CR>", default_options) -- zoom
-map("n", "<S-A-e>", ":wincmd =<CR>", default_options) -- equal splits
+-- map("n", "<S-A-e>", ":wincmd =<CR>", default_options) -- equal splits
 
 -- map("n", "+", ":res +5<CR>", default_options)
 -- map("n", "-", ":res -5<CR>", default_options)
