4ac24e46917bc27be1c45ac5b271c735d9fbd8b5
tim goh <timslgoh@gmail.com>
2023-03-18 11:51:20 +0800

    fix(vim/luasnip): fix hot reload
    https://github.com/L3MON4D3/LuaSnip/issues/518
    luasnip does not hot reload if file being edited does not belong to the
    directory specified in the luasnip setup
    the workaround fix is to point the luasnip snippets folder to my actual
    config folder and not the symlinked XDG_CONFIG folder

--- config/nvim/lua/setup/luasnip.lua
+++ config/nvim/lua/setup/luasnip.lua
@@ -30,7 +30,7 @@ local ms = ls.multi_snippet
 -- {{{
 
 -- require("luasnip.loaders.from_vscode").lazy_load()
-require("luasnip.loaders.from_lua").load({paths = "~/.config/nvim/snippets/"})
+require("luasnip.loaders.from_lua").load({paths = "~/dotfiles/config/nvim/snippets/"})
 require("luasnip").config.setup({store_selection_keys = "<A-p>"})
 
 vim.cmd([[command! LuaSnipEdit :lua require("luasnip.loaders.from_lua").edit_snippet_files()]]) -- }}}
