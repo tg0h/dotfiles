704ea93edf2117779f8d6d042c6bc41156f0c216
tim goh <timslgoh@gmail.com>
2023-03-23 08:48:23 +0800

    feat(vim/luasnip): change change choice keymap from C-s to C-n

--- config/nvim/lua/setup/luasnip.lua
+++ config/nvim/lua/setup/luasnip.lua
@@ -63,7 +63,7 @@ vim.keymap.set({"i", "s"}, "<c-u>", '<cmd>lua require("luasnip.extras.select_cho
 vim.keymap.set({"i", "s"}, "<C-t>", function() if ls.expand_or_jumpable() then ls.expand_or_jump() end end,
                {silent = true})
 vim.keymap.set({"i", "s"}, "<C-h>", function() if ls.jumpable(-1) then ls.jump(-1) end end, {silent = true})
-vim.keymap.set({"i", "s"}, "<C-s>", function() if ls.choice_active() then ls.change_choice(1) end end)
+vim.keymap.set({"i", "s"}, "<C-n>", function() if ls.choice_active() then ls.change_choice(1) end end)
 
 -- jumping
 -- also used by quickfix down/up
