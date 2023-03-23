a8022d0fe4052aaa501e7a79eeeb5ff6d59e67ff
tim goh <timslgoh@gmail.com>
2023-03-23 08:16:50 +0800

    feat(vim/luasnip): change keymaps to jump nodes and change choice

--- config/nvim/lua/setup/luasnip.lua
+++ config/nvim/lua/setup/luasnip.lua
@@ -39,10 +39,11 @@ vim.cmd([[command! LuaSnipEdit :lua require("luasnip.loaders.from_lua").edit_sni
 local types = require("luasnip.util.types")
 ls.config.set_config({
     history = true, -- keep around last snippet local to jump back
+    -- update dynamic snippets as you type
     updateevents = "TextChanged,TextChangedI", -- update changes as you type
     enable_autosnippets = true,
     ext_opts = {
-        [types.choiceNode] = {active = {virt_text = {{"●", "GruvboxOrange"}}}}
+        -- [types.choiceNode] = {active = {virt_text = {{"●", "GruvboxOrange"}}}}
         -- [types.insertNode] = {
         --  active = {
         --    virt_text = { { "●", "GruvboxBlue" } },
@@ -53,41 +54,33 @@ ls.config.set_config({
 
 -- Key Mapping --{{{
 
-vim.keymap.set({"i", "s"}, "<c-s>", "<Esc>:w<cr>")
+-- vim.keymap.set({"i", "s"}, "<c-s>", "<Esc>:w<cr>")
+
+-- show picker via vim.ui.select to select choice
 vim.keymap.set({"i", "s"}, "<c-u>", '<cmd>lua require("luasnip.extras.select_choice")()<cr><C-c><C-c>')
 
 -- this is the expand key
-vim.keymap.set({"i", "s"}, "<a-p>", function() if ls.expand_or_jumpable() then ls.expand() end end, {silent = true})
--- vim.keymap.set({ "i", "s" }, "<C-k>", function()
--- 	if ls.expand_or_jumpable() then
--- 		ls.expand_or_jump()
--- 	end
--- end, { silent = true })
--- vim.keymap.set({ "i", "s" }, "<C-j>", function()
--- 	if ls.jumpable() then
--- 		ls.jump(-1)
--- 	end
--- end, { silent = true })
-
-vim.keymap.set({"i", "s"}, "<A-y>", "<Esc>o", {silent = true})
+vim.keymap.set({"i", "s"}, "<C-t>", function() if ls.expand_or_jumpable() then ls.expand_or_jump() end end,
+               {silent = true})
+vim.keymap.set({"i", "s"}, "<C-h>", function() if ls.jumpable(-1) then ls.jump(-1) end end, {silent = true})
+vim.keymap.set({"i", "s"}, "<C-s>", function() if ls.choice_active() then ls.change_choice(1) end end)
 
 -- jumping
 -- also used by quickfix down/up
-vim.keymap.set({"i", "s"}, "<M-j>", function() if ls.jumpable(1) then ls.jump(1) end end, {silent = true})
-vim.keymap.set({"i", "s"}, "<M-k>", function() if ls.jumpable(-1) then ls.jump(-1) end end, {silent = true})
-
--- cycle through choices
-vim.keymap.set({"i", "s"}, "<a-l>", function()
-    if ls.choice_active() then
-        ls.change_choice(1)
-    else
-        -- print current time
-        local t = os.date("*t")
-        local time = string.format("%02d:%02d:%02d", t.hour, t.min, t.sec)
-        print(time)
-    end
-end)
-vim.keymap.set({"i", "s"}, "<a-h>", function() if ls.choice_active() then ls.change_choice(-1) end end) -- }}}
+-- vim.keymap.set({"i", "s"}, "<M-j>", function() if ls.jumpable(1) then ls.jump(1) end end, {silent = true})
+-- vim.keymap.set({"i", "s"}, "<M-k>", function() if ls.jumpable(-1) then ls.jump(-1) end end, {silent = true})
+-- -- cycle through choices
+-- vim.keymap.set({"i", "s"}, "<a-l>", function()
+--     if ls.choice_active() then
+--         ls.change_choice(1)
+--     else
+--         -- print current time
+--         local t = os.date("*t")
+--         local time = string.format("%02d:%02d:%02d", t.hour, t.min, t.sec)
+--         print(time)
+--     end
+-- end)
+-- vim.keymap.set({"i", "s"}, "<a-h>", function() if ls.choice_active() then ls.change_choice(-1) end end) -- }}}
 
 -- More Settings --
 
