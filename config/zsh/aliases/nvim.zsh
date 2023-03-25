f968e9623ff202f5fd02fb6d4a7cf7277147ce62
tim goh <timslgoh@gmail.com>
2023-03-25 12:34:32 +0800

    feat(alias/nvim): ucan edits the neovim alias file

--- config/zsh/aliases/nvim.zsh
+++ config/zsh/aliases/nvim.zsh
@@ -1,2 +1,6 @@
+alias ucan='nvim ~/dotfiles/config/zsh/aliases/nvim.zsh'
+
 alias unp='nvim ~/dotfiles/config/nvim/lua/plugins.lua'
 alias uns='nvim ~/dotfiles/config/nvim/lua/setup/luasnip.lua'
+
+alias unl='nvim ~/dotfiles/config/nvim/snippets/lua.lua'
