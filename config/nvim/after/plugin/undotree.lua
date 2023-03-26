7610e8426608e1c9b50b542e94a323ccc1a9d61c
tim goh <timslgoh@gmail.com>
2023-03-26 13:45:21 +0800

    feat(vim/after): S-C-A-u opens undotree

---
+++ config/nvim/after/plugin/undotree.lua
@@ -0,0 +1,2 @@
+-- https://vi.stackexchange.com/questions/35089/how-to-robustly-use-d-mappings-in-terminal-nvim
+vim.keymap.set('n', '<S-C-A-u>', ':UndotreeToggle<CR>', default_options) -- undotree
--- config/nvim/lua/maps/plugins/undo-tree.lua
+++ config/nvim/lua/maps/plugins/undo-tree.lua
@@ -1,5 +1,5 @@
-local map = vim.api.nvim_set_keymap
-default_options = { noremap = true, silent = true }
-expr_options = { noremap = true, expr = true, silent = true }
-
-map('n', '<F11>', ':UndotreeToggle<CR>', default_options) -- undotree
+-- local map = vim.api.nvim_set_keymap
+-- default_options = { noremap = true, silent = true }
+-- expr_options = { noremap = true, expr = true, silent = true }
+--
+-- map('n', '<S-C-A-u>', ':UndotreeToggle<CR>', default_options) -- undotree
