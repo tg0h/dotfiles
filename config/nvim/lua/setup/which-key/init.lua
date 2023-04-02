46fc85fb08ef9e04b6c514142e8ba36279b9e238
tim goh <timslgoh@gmail.com>
2023-04-02 15:05:49 +0800

    feat(vim/which-key): reduce trigger time of which-key registers plugin

--- config/nvim/lua/setup/which-key/init.lua
+++ config/nvim/lua/setup/which-key/init.lua
@@ -34,18 +34,19 @@ require('which-key').setup({
     group = '+', -- symbol prepended to a group
   },
   popup_mappings = {
-    scroll_down = '<c-d>', -- binding to scroll down inside the popup
-    scroll_up = '<c-u>', -- binding to scroll up inside the popup
+    scroll_down = '<C-d>', -- binding to scroll down inside the popup
+    scroll_up = '<C-u>', -- binding to scroll up inside the popup
   },
   window = {
     border = 'none', -- none, single, double, shadow
     position = 'bottom', -- bottom, top
-    margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
-    padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
-    winblend = 0,
+    -- margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
+    -- padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
+    winblend = 10,
   },
   layout = {
-    height = { min = 4, max = 25 }, -- min and max height of the columns
+    -- increase height for eg which key builtin registers plugin
+    height = { min = 4, max = 100 }, -- min and max height of the columns
     width = { min = 20, max = 50 }, -- min and max width of the columns
     spacing = 3, -- spacing between columns
     align = 'left', -- align columns left, center or right
@@ -53,8 +54,22 @@ require('which-key').setup({
   ignore_missing = false, -- enable this to hide mappings for which you didn't specify a label
   hidden = { '<silent>', '<cmd>', '<Cmd>', '<CR>', 'call', 'lua', '^:', '^ ' }, -- hide mapping boilerplate
   show_help = true, -- show help message on the command line when the popup is visible
+  show_keys = true, -- show the currently pressed key and its label as a message in the command line
   triggers = 'auto', -- automatically setup triggers
   -- triggers = {"<leader>"} -- or specify a list manually
+  -- list of triggers, where WhichKey should not wait for timeoutlen and show immediately
+  triggers_nowait = {
+    -- marks
+    '`',
+    "'",
+    'g`',
+    "g'",
+    -- registers
+    '"',
+    '<c-r>',
+    -- spelling
+    'z=',
+  },
   triggers_blacklist = {
     -- list of mode / prefixes that should never be hooked by WhichKey
     -- this is mostly relevant for key maps that start with a native binding
