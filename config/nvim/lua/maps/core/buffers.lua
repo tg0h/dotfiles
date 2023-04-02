90787fa0721a5993dc4a9a5e5b15f2ec1df40cbc
tim goh <timslgoh@gmail.com>
2023-04-02 16:18:59 +0800

    fix(vim/maps): fix close/move left/right buffer

--- config/nvim/lua/maps/core/buffers.lua
+++ config/nvim/lua/maps/core/buffers.lua
@@ -4,21 +4,22 @@ expr_options = { noremap = true, expr = true, silent = true }
 
 -- Buffers
 -- map("n", "<A-Esc>", ":e#<CR>", default_options) -- edit previously opened buffer (or C-^)
-map('n', '<A-Space>', ':e#<CR>', default_options) -- edit previously opened buffer (or C-^)
+-- map('n', '<A-CR>', ':e#<CR>', default_options) -- edit previously opened buffer (or C-^)
 
-map('n', '<A-[>', ':BufferLineGoToBuffer 1<CR>', default_options) -- Go to Buffer 1
-map('n', '<A-S-[>', ':BufferLineGoToBuffer 2<CR>', default_options) -- A-{
-map('n', '<A-S-]>', ':BufferLineGoToBuffer 3<CR>', default_options) -- A-}
-map('n', '<A-S-9>', ':BufferLineGoToBuffer 4<CR>', default_options) -- A-(
-map('n', '<A-=>', ':BufferLineGoToBuffer 5<CR>', default_options)
+-- map('n', '<A-1>', ':BufferLineGoToBuffer 1<CR>', default_options) -- Go to Buffer 1
+-- map('n', '<A-2>', ':BufferLineGoToBuffer 2<CR>', default_options) -- Go to Buffer 2
+-- map('n', '<A-S-[>', ':BufferLineGoToBuffer 2<CR>', default_options) -- A-{
+-- map('n', '<A-S-]>', ':BufferLineGoToBuffer 3<CR>', default_options) -- A-}
+-- map('n', '<A-S-9>', ':BufferLineGoToBuffer 4<CR>', default_options) -- A-(
+-- map('n', '<A-=>', ':BufferLineGoToBuffer 5<CR>', default_options)
 
 map('n', '<A-g>', ':BufferLineCyclePrev<CR>', default_options) -- Go to left buffer 'tab'
 map('n', '<A-r>', ':BufferLineCycleNext<CR>', default_options) -- Go to right buffer 'tab'
 map('n', '<A-c>', ':Bdelete!<CR>', default_options) -- close buffer
 
-map('n', '<A-S-0>', ':BufferLineMovePrev<CR>', default_options) -- A-)
-map('n', '<A-]>', ':BufferLineMoveNext<CR>', default_options)
+map('n', '<S-A-7>', ':BufferLineMovePrev<CR>', default_options) -- A-)
+map('n', '<S-A-[>', ':BufferLineMoveNext<CR>', default_options)
 
-map('n', '<A-S-8>', ':BufferLineCloseLeft<CR>', default_options) -- A-* close all buffers to the left
-map('n', '<A-S-=>', ':BufferLineCloseLeft<CR>:BufferLineCloseRight<CR>', default_options) -- A-+ close all but selected buffer
-map('n', '<A-S-1>', ':BufferLineCloseRight<CR>', default_options) -- A-! close all buffers to the right
+map('n', '<A-7>', ':BufferLineCloseLeft<CR>', default_options) -- A-* close all buffers to the left
+map('n', '<A-8>', ':BufferLineCloseLeft<CR>:BufferLineCloseRight<CR>', default_options) -- A-+ close all but selected buffer
+map('n', '<A-[>', ':BufferLineCloseRight<CR>', default_options) -- A-! close all buffers to the right
