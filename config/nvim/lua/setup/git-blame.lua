9f2cd77c329abf6978439862b7253eff8926c804
tim goh <timslgoh@gmail.com>
2023-04-07 19:45:12 +0800

    feat(vim/git-blame): M-x toggles git blame

--- config/nvim/lua/setup/git-blame.lua
+++ config/nvim/lua/setup/git-blame.lua
@@ -4,3 +4,5 @@ g.gitblame_enabled = 0
 g.gitblame_message_template = '<author> • <summary> • <date> • <sha>'
 
 g.gitblame_date_format = '%r - %a %d %b %y %X'
+
+vim.keymap.set('n', '<M-x>', '<CMD>GitBlameToggle<CR>', { desc = 'git blame toggle' })
