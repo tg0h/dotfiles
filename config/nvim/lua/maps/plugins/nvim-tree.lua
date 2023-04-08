a5a641873e21e3ef683aba4bdf955e8863acd69c
tim goh <timslgoh@gmail.com>
2023-04-08 15:40:37 +0800

    feat(vim/maps): M-CR toggles nvim tree and keeps it open

--- config/nvim/lua/maps/plugins/nvim-tree.lua
+++ config/nvim/lua/maps/plugins/nvim-tree.lua
@@ -1,7 +1,18 @@
+local api = require('nvim-tree.api')
 local map = vim.api.nvim_set_keymap
 default_options = { noremap = true, silent = true }
 expr_options = { noremap = true, expr = true, silent = true }
 
 -- map("n", "<A-u>", ":NvimTreeFocus<CR>", default_options) -- Focus is better than Toggle
-map('n', '<A-m>', ':NvimTreeToggle<CR>', default_options)
-map('n', '<A-b>', ':NvimTreeFindFile<CR>', default_options)
+
+vim.keymap.set('n', '<A-m>', ':NvimTreeToggle<CR>', { desc = 'nvim tree toggle' })
+vim.keymap.set('n', '<A-b>', ':NvimTreeFindFile<CR>', { desc = 'nvim tree find file' })
+
+vim.keymap.set('n', '<M-CR>', function()
+  if vim.fn.bufname():match('NvimTree_') then
+    -- previous window
+    vim.cmd.wincmd('p')
+  else
+    api.tree.open({ find_file = true })
+  end
+end, { desc = 'nvim-tree: toggle' })
