4eb502ae679380fbb4deb5d613799b3d39d29e37
tim goh <timslgoh@gmail.com>
2023-04-18 17:17:19 +0800

    refactor(vim/maps): use new keyboard number layout

--- config/nvim/lua/maps/core/buffers.lua
+++ config/nvim/lua/maps/core/buffers.lua
@@ -1,28 +1,38 @@
-local map = vim.api.nvim_set_keymap
-default_options = { noremap = true, silent = true }
-expr_options = { noremap = true, expr = true, silent = true }
-
+-- local vim.keymap.set = vim.api.nvim_set_keymap
+-- default_options = { norevim.keymap.set = true, silent = true }
+-- expr_options = { norevim.keymap.set = true, expr = true, silent = true }
+--
 -- Buffers
--- map("n", "<A-Esc>", ":e#<CR>", default_options) -- edit previously opened buffer (or C-^)
--- map('n', '<A-CR>', ':e#<CR>', default_options) -- edit previously opened buffer (or C-^)
+-- vim.keymap.set("n", "<A-Esc>", ":e#<CR>", default_options) -- edit previously opened buffer (or C-^)
+-- vim.keymap.set('n', '<A-CR>', ':e#<CR>', default_options) -- edit previously opened buffer (or C-^)
 
-map('n', '<A-1>', ':BufferLineGoToBuffer 1<CR>', default_options) -- Go to Buffer 1
-map('n', '<A-2>', ':BufferLineGoToBuffer 2<CR>', default_options) -- Go to Buffer 2
-map('n', '<A-3>', ':BufferLineGoToBuffer 3<CR>', default_options) -- Go to Buffer 2
-map('n', '<A-4>', ':BufferLineGoToBuffer 4<CR>', default_options) -- Go to Buffer 2
-map('n', '<A-5>', ':BufferLineGoToBuffer 5<CR>', default_options) -- Go to Buffer 2
-map('n', '<A-6>', ':BufferLineGoToBuffer 6<CR>', default_options) -- Go to Buffer 2
-map('n', '<A-7>', ':BufferLineGoToBuffer 7<CR>', default_options) -- Go to Buffer 2
-map('n', '<A-8>', ':BufferLineGoToBuffer 8<CR>', default_options) -- Go to Buffer 2
-map('n', '<A-9>', ':BufferLineGoToBuffer 9<CR>', default_options) -- Go to Buffer 2
+-- nnorevim.keymap.set <silent><leader>1 <cmd>lua require("bufferline").go_to_buffer(1, true)<cr>
 
-map('n', '<A-g>', ':BufferLineCyclePrev<CR>', default_options) -- Go to left buffer 'tab'
-map('n', '<A-r>', ':BufferLineCycleNext<CR>', default_options) -- Go to right buffer 'tab'
-map('n', '<A-c>', ':Bdelete!<CR>', default_options) -- close buffer
+-- go to nth visible buffer, not by absolute position
+vim.keymap.set('n', '<A-1>', ':BufferLineGoToBuffer 1<CR>', { desc = 'go to buffer 1' })
+vim.keymap.set('n', '<A-2>', ':BufferLineGoToBuffer 2<CR>', { desc = 'go to buffer 2' })
+vim.keymap.set('n', '<A-3>', ':BufferLineGoToBuffer 3<CR>', { desc = 'go to buffer 3' })
+vim.keymap.set('n', '<A-4>', ':BufferLineGoToBuffer 4<CR>', { desc = 'go to buffer 4' })
+vim.keymap.set('n', '<A-5>', ':BufferLineGoToBuffer 5<CR>', { desc = 'go to buffer 5' })
+vim.keymap.set('n', '<A-6>', ':BufferLineGoToBuffer 6<CR>', { desc = 'go to buffer 6' })
+-- vim.keymap.set('n', '<A-7>', ':BufferLineGoToBuffer 7<CR>', default_options)
+-- vim.keymap.set('n', '<A-8>', ':BufferLineGoToBuffer 8<CR>', default_options)
+-- vim.keymap.set('n', '<A-9>', ':BufferLineGoToBuffer 9<CR>', default_options)
 
-map('n', '<S-A-7>', ':BufferLineMovePrev<CR>', default_options) -- A-)
-map('n', '<S-A-[>', ':BufferLineMoveNext<CR>', default_options)
+vim.keymap.set('n', '<S-A-4>', ':BufferLineGoToBuffer -1<CR>', { desc = 'go to last buffer' }) -- A-$
 
-map('n', '<A-7>', ':BufferLineCloseLeft<CR>', default_options) -- A-* close all buffers to the left
-map('n', '<A-8>', ':BufferLineCloseLeft<CR>:BufferLineCloseRight<CR>', default_options) -- A-+ close all but selected buffer
-map('n', '<A-6>', ':BufferLineCloseRight<CR>', default_options) -- A-! close all buffers to the right A-[ used by treesitter text objects
+vim.keymap.set('n', '<A-g>', ':BufferLineCyclePrev<CR>', { desc = 'go to previous buffer' }) -- go to left buffer
+vim.keymap.set('n', '<A-r>', ':BufferLineCycleNext<CR>', { desc = 'go to next buffer' }) -- go to right buffer
+vim.keymap.set('n', '<A-c>', ':Bdelete!<CR>', { desc = 'go to last buffer' }) -- close
+
+vim.keymap.set('n', '<S-A-9>', ':BufferLineMovePrev<CR>', { desc = 'move to previous buffer' })
+vim.keymap.set('n', '<S-A-[>', ':BufferLineMoveNext<CR>', { desc = 'move to next buffer' })
+
+vim.keymap.set('n', '<A-8>', ':BufferLineCloseLeft<CR>', { desc = 'close buffers to the left' })
+vim.keymap.set('n', '<A-9>', ':BufferLineCloseRight<CR>', { desc = 'close buffers to the right' })
+vim.keymap.set(
+  'n',
+  '<A-0>',
+  ':BufferLineCloseLeft<CR>:BufferLineCloseRight<CR>',
+  { desc = 'close all except current buffer' }
+)
