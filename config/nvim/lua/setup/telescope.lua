5641aa6ec735409d5a9758fe462cd36a28c252bb
tim goh <timslgoh@gmail.com>
2023-04-18 17:17:41 +0800

    fix(vim/which-key): fix grep_string layout

--- config/nvim/lua/setup/telescope.lua
+++ config/nvim/lua/setup/telescope.lua
@@ -16,6 +16,21 @@ M.my_grep_string = function()
   })
 end
 
+M.my_grep_string_no_input = function()
+  require('telescope.builtin').grep_string({
+    hidden = false,
+    layout_strategy = 'vertical',
+    layout_config = {
+      height = 0.99,
+      -- anchor = 'E',
+      width = 0.9,
+      prompt_position = 'top',
+      preview_height = 0.5,
+      mirror = true,
+    },
+  })
+end
+
 M.my_git_files = function()
   require('telescope.builtin').git_files({
     hidden = false,
