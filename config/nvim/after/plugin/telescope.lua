d9ff3acb05fba3b08ab39407ffba960b13a4a201
tim goh <timslgoh@gmail.com>
2023-05-01 14:26:00 +0800

    refactor(vim/plugin/telescope): prettify config

--- config/nvim/after/plugin/telescope.lua
+++ config/nvim/after/plugin/telescope.lua
@@ -75,10 +75,15 @@ local ext = require('telescope').extensions
 
 local builtin = require('telescope.builtin')
 
+--------------------------------------------------------------------------------------------------
+-- MAPS ----------------------------------------------------------------------------------------
 -- project
 vim.keymap.set('n', '<C-p>', require('setup/telescope').my_git_files, { desc = 'git files' })
+vim.keymap.set('n', '<S-C-->', builtin.commands)
 
--- files
+--------------------------------------------------------------------------------------------------
+-- LEADER ----------------------------------------------------------------------------------------
+-- files #########################################################################################
 vim.keymap.set('n', '<LEADER>ff', builtin.find_files, { desc = 'find files' })
 vim.keymap.set('n', '<LEADER>fh', function()
   builtin.find_files({
@@ -93,7 +98,7 @@ vim.keymap.set('n', '<LEADER>fr', function()
 end, { desc = 'find frecent files' })
 vim.keymap.set('n', '<LEADER>fo', builtin.oldfiles, { desc = 'old files' })
 
--- neovim
+-- neovim #########################################################################################
 vim.keymap.set('n', '<LEADER>na', function()
   vim.cmd.Telescope('heading')
 end, { desc = 'headings' })
@@ -112,15 +117,18 @@ vim.keymap.set('n', '<LEADER>nQ', builtin.quickfixhistory, { desc = 'quickfix hi
 -- vim.keymap.set('n', '<LEADER>nC', builtin.colorscheme, { desc = 'colorschemes' })
 -- vim.keymap.set('n', '<LEADER>ny', builtin.symbols, { desc = 'emojis' })
 
+-- search #########################################################################################
 vim.keymap.set('n', '<LEADER>st', builtin.live_grep, { desc = 'telescope live_grep' })
-vim.keymap.set('n', '<leader>ss', function() -- search text provided via input
+vim.keymap.set('n', '<LEADER>ss', function() -- search text provided via input
   require('setup/telescope').my_grep_string_no_input()
 end, { desc = 'telescope my grep string no input' })
-vim.keymap.set('n', '<leader>sp', function() -- search text provided via input
+vim.keymap.set('n', '<LEADER>sp', function() -- search text provided via input
   require('setup/telescope').my_grep_string()
 end, { desc = 'telescope my grep string with input' })
 vim.keymap.set('n', '<LEADER>sb', builtin.current_buffer_fuzzy_find, { desc = 'telescope current buffer fuzzy find' })
 vim.keymap.set('n', '<LEADER>nh', builtin.help_tags, { desc = 'neovim help' })
+local multi_rg = require('tg.telescope-multi-rg')
+vim.keymap.set('n', '<leader>sg', multi_rg, { desc = 'tj multi rg' })
 
 vim.keymap.set('n', '<LEADER>sM', function()
   harpoon.marks()
@@ -130,8 +138,6 @@ end, { desc = 'harpoon marks' })
 --   builtin.grep_string({ search = vim.fn.input('Grep > ') })
 -- end)
 
-vim.keymap.set('n', '<S-C-->', builtin.commands)
-
 -- workspace diagnostics
 vim.keymap.set('n', '<S-M-f>', function()
   builtin.diagnostics({ layout_strategy = 'vertical', layout_config = { width = 0.5 } })
@@ -140,9 +146,10 @@ end)
 local multi_rg = require('tg.telescope-multi-rg')
 vim.keymap.set('n', '<leader>sg', multi_rg, { desc = 'tj multi rg' })
 
-vim.keymap.set('n', '<leader>cd', function()
+-- config #########################################################################################
+vim.keymap.set('n', '<LEADER>cd', function()
   require('setup/telescope').search_dotfiles()
 end, { desc = 'search dotfiles' })
-vim.keymap.set('n', '<leader>cn', function()
+vim.keymap.set('n', '<LEADER>cn', function()
   require('setup/telescope').search_neovim_dotfiles()
 end, { desc = 'search dotfiles' })
