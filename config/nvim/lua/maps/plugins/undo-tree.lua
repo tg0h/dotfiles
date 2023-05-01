2c6581b510971e934a8e648230ba6326fa42f3bb
tim goh <timslgoh@gmail.com>
2023-05-01 12:22:05 +0800

    refactor(vim/maps): remove undo-tree

--- config/nvim/lua/maps/plugins/undo-tree.lua
+++
@@ -1,5 +0,0 @@
--- local map = vim.api.nvim_set_keymap
--- default_options = { noremap = true, silent = true }
--- expr_options = { noremap = true, expr = true, silent = true }
---
--- map('n', '<S-C-A-u>', ':UndotreeToggle<CR>', default_options) -- undotree
