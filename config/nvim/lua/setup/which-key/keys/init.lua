764f685682b7c1903e416a2ddb67aaaf7b0a1639
tim goh <timslgoh@gmail.com>
2023-05-01 14:32:54 +0800

    refactor(vim/which-key): remove window

--- config/nvim/lua/setup/which-key/keys/init.lua
+++ config/nvim/lua/setup/which-key/keys/init.lua
@@ -18,7 +18,17 @@ local M = {
   g = require('setup.which-key.keys.git'),
   l = require('setup.which-key.keys.lsp'),
 
-  w = require('setup.which-key.keys.window'),
+  w = {
+    name = 'Window',
+    a = { '<C-w><C-o>', 'Close all other splits' },
+    q = { '<cmd>:q<cr>', 'Close' },
+    s = { '<cmd>:split<cr>', 'Horizontal Split' },
+    t = { '<c-w>t', 'Move to new tab' },
+    ['='] = { '<c-w>=', 'Equally size' },
+    v = { '<cmd>:vsplit<cr>', 'Vertical Split' },
+    w = { '<c-w>x', 'Swap' },
+    z = { '<C-w>_<C-w>|<CR>', 'Zoom Split' },
+  },
   z = require('setup.which-key.keys.vim-dadbod-ui'),
 }
 return M
