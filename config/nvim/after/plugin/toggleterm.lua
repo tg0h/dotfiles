2cb0df3acfd787fafee88a636db8a55128517975
tim goh <timslgoh@gmail.com>
2023-05-06 13:48:14 +0800

    feat(vim/plugin/toggleterm): map C-- to toggleterm

--- config/nvim/after/plugin/toggleterm.lua
+++ config/nvim/after/plugin/toggleterm.lua
@@ -2,17 +2,18 @@ require('toggleterm').setup({
   -- size can be a number or function which is passed the current terminal
   size = function(term)
     if term.direction == 'horizontal' then
-      return 50
+      return 30
       -- return vim.o.rows * 0.4
     elseif term.direction == 'vertical' then
       return vim.o.columns * 0.4
     end
   end,
-  -- open_mapping = [[<C-i>]], -- NOTE: opens toggleterm!
+  -- kitty maps C-- to C-], so use C-] to open toggleterm
+  open_mapping = [[<C-]>]], -- NOTE: opens toggleterm!
   hide_numbers = true, -- hide the number column in toggleterm buffers
   shade_filetypes = {},
-  shade_terminals = true,
-  shading_factor = '1', -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
+  autochdir = false, -- when neovim changes it current directory the terminal will change it's own when next it's opened
+  shade_terminals = true, -- NOTE: this option takes priority over highlights specified so if you specify Normal highlights you should set this to false
   start_in_insert = true,
   insert_mappings = true, -- whether or not the open mapping applies in insert mode
   persist_size = true,
@@ -31,11 +32,18 @@ require('toggleterm').setup({
     winblend = 3,
     highlights = { border = 'Normal', background = 'Normal' },
   },
+  winbar = {
+    enabled = false,
+    name_formatter = function(term) --  term: Terminal
+      return term.name
+    end,
+  },
 })
 local Terminal = require('toggleterm.terminal').Terminal
 local lazygit = Terminal:new({ cmd = 'lazygit', direction = 'float', hidden = true })
 function _lazygit_toggle()
   lazygit:toggle()
 end
+
 vim.api.nvim_set_keymap('n', '<leader>gl', '<cmd>lua _lazygit_toggle()<CR>', { noremap = true, silent = true }) -- start lazygit
 vim.api.nvim_set_keymap('t', '<ESC>', '<C-\\><C-n>', { noremap = true, silent = true }) -- back to normal mode in Terminal
