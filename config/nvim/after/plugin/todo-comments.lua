f5e5d20c18f7a54e8baf617482822875e7f737cd
tim goh <timslgoh@gmail.com>
2023-05-06 08:20:28 +0800

    fix(vim/plugin/todo-comments): correct filename

--- config/nvim/after/plugin/todo-comments.lua
+++ config/nvim/after/plugin/todo-comments.lua
@@ -47,3 +47,17 @@ require('todo-comments').setup({
     -- pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
   },
 })
+
+-- vim.keymap.set('n', '<S-C-r>', function()
+--   require('todo-comments').jump_next()
+-- end, { desc = 'Next todo comment' })
+--
+-- vim.keymap.set('n', '<S-C-g>', function()
+--   require('todo-comments').jump_prev()
+-- end, { desc = 'Previous todo comment' })
+--
+-- -- You can also specify a list of valid jump keywords
+--
+-- vim.keymap.set('n', ']t', function()
+--   require('todo-comments').jump_next({ keywords = { 'ERROR', 'WARNING' } })
+-- end, { desc = 'Next error/warning todo comment' })
