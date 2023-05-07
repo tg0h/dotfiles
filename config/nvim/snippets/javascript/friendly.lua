e8cadf55c7fe2f14911d8f75d83806ad0e3bf1bc
tim goh <timslgoh@gmail.com>
2023-05-07 08:02:25 +0800

    feat(vim/snippets/javascript): add ao

--- config/nvim/snippets/javascript/friendly.lua
+++ config/nvim/snippets/javascript/friendly.lua
@@ -12,6 +12,7 @@ return {
       }
     )
   ),
+  s('ao', fmt([[console.log({})]], { i(1, '') })),
   s('c', fmt([[const {} = {}]], { i(1), i(0) })),
   s('l', { t('let '), i(1, '') }),
   s('js', fmt([[JSON.stringify({}){}]], { i(1), i(0) })),
