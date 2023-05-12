3ec24402f71e8fc4b98fec9b1dca855ca178ee04
tim goh <timslgoh@gmail.com>
2023-05-12 16:42:54 +0800

    feat(vim/after): add harpoon menu specific keymap to move menu items

--- config/nvim/after/ftplugin/harpoon.lua
+++ config/nvim/after/ftplugin/harpoon.lua
@@ -7,3 +7,6 @@ vim.wo.relativenumber = false
 local bufnr = vim.api.nvim_get_current_buf()
 vim.fn.sign_define('my_sign', { text = ' ', texthl = 'Search', linehl = '', numhl = '' })
 vim.fn.sign_place(0, 'my_sign', 'my_sign', bufnr, { lnum = 5, priority = 100 })
+
+vim.api.nvim_buf_set_keymap(0, 'n', '<C-j>', ':move +1<CR>', { desc = 'move harpoon menu item down' })
+vim.api.nvim_buf_set_keymap(0, 'n', '<C-k>', ':move -2<CR>', { desc = 'move harpoon menu item up' })
