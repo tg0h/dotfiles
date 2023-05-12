a502df71fb0a26ec3e19a61b894263d720e45863
tim goh <timslgoh@gmail.com>
2023-05-12 16:26:13 +0800

    feat(vim/after): highlight 5th line in harpoon menu

--- config/nvim/after/ftplugin/harpoon.lua
+++ config/nvim/after/ftplugin/harpoon.lua
@@ -2,3 +2,8 @@
 vim.wo.cursorline = true
 -- show absolute line numbers for easier reference
 vim.wo.relativenumber = false
+
+-- highlight the 5th line
+local bufnr = vim.api.nvim_get_current_buf()
+vim.fn.sign_define('my_sign', { text = ' ', texthl = 'Search', linehl = '', numhl = '' })
+vim.fn.sign_place(0, 'my_sign', 'my_sign', bufnr, { lnum = 5, priority = 100 })
