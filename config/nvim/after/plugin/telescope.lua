49808d77309387259aa920fdb603c5be6d7d648a
tim goh <timslgoh@gmail.com>
2023-07-15 16:42:36 +0800

    refactor(vim/plugin/telescope): comment out git worktree

--- config/nvim/after/plugin/telescope.lua
+++ config/nvim/after/plugin/telescope.lua
@@ -12,7 +12,7 @@ local file_browser = telescope.load_extension('file_browser')
 telescope.load_extension('heading') -- markdown, help headings
 -- telescope.load_extension "projects" -- recent projects
 local harpoon = telescope.load_extension('harpoon')
-telescope.load_extension('git_worktree')
+-- telescope.load_extension('git_worktree')
 local neoclip = telescope.load_extension('neoclip')
 telescope.load_extension('gitdiffer')
 telescope.load_extension('frecency')
