a3f87010a47875ba57f067c1453401b541a3603f
tim goh <timslgoh@gmail.com>
2023-08-06 12:42:48 +0800

    feat(kitty): colour wiki tab when opening for first time

--- config/kitty/tab_switcher.py
+++ config/kitty/tab_switcher.py
@@ -48,41 +48,23 @@ def handle_result(args: List[str], answer: str, target_window_id: int, boss: Bos
     # for tab in tm.tabs:
     #     print(f'tab id is {getattr(tab,"id","oops")}')
 
-    colourOrange = int(to_color('orange', validate=True))
-    colourPink = int(to_color('pink', validate=True))
-    colourBlack = int(to_color('black', validate=True))
-
     is_wiki_focussed=False
     if boss.active_tab.title == 'wiki' or boss.active_tab.title == 'candy_wiki':
         is_wiki_focussed=True
 
-    tabs = boss.match_tabs('title:^wiki or title:^candy_wiki')
-    # print(f'tabs is {tabs}')
-    tab = next(tabs, None) # default value for generator to None instead of throwing StopIteration error
+    tab = get_wiki_tab(boss)
     # print(f'tab is {tab}')
     # if tab is not None:
     #     tabTitle=tab.title
     #     print(f'tab title is {tab.title}')
 
     # print(f'args is {args}')
-    wikiArg=args[1] # these are the args passed to the tab_switcher kitten
-    if wikiArg == 'wiki':
-        wikiFolder='/Users/tim/src/me/wiki'
-        wikiTitle='wiki'
-    else:
-        wikiFolder='/Users/tim/Documents/candy/wiki'
-        wikiTitle='candy_wiki'
 
     if tab:
-        # tab.inactive_bg = colourOrange
-        tab.active_fg = colourBlack
-        if tab.title == 'wiki':
-            tab.inactive_fg = colourOrange
-            tab.active_bg = colourOrange
-        else:
-            tab.inactive_fg = colourPink
-            tab.active_bg = colourPink
-        redraw_tab_bar(boss)
+        colour_tab(boss, tab)
+        is_wiki_focussed=False
+        if boss.active_tab.title == 'wiki' or boss.active_tab.title == 'candy_wiki':
+            is_wiki_focussed=True
         if not is_wiki_focussed:
             boss.set_active_tab(tab)
         else:
@@ -94,9 +76,12 @@ def handle_result(args: List[str], answer: str, target_window_id: int, boss: Bos
             # unlike goto_tab -1, boss.goto_tab uses 0 instead
             boss.goto_tab(0) # go to previous active tab
     else:
+        wikiArg=args[1] # these are the args passed to the tab_switcher kitten
         if wikiArg == 'wiki':
+            wikiFolder='/Users/tim/src/me/wiki'
             wikiTitle='wiki'
         else:
+            wikiFolder='/Users/tim/Documents/candy/wiki'
             wikiTitle='candy_wiki'
         boss.launch(
         "--type=tab",
@@ -106,3 +91,26 @@ def handle_result(args: List[str], answer: str, target_window_id: int, boss: Bos
         "-c",
         "lua open_latest_changed_file()", # run my lua script
         )
+        tab = get_wiki_tab(boss)
+        if tab:
+            colour_tab(boss, tab)
+
+def get_wiki_tab(boss: Boss):
+    tabs = boss.match_tabs('title:^wiki or title:^candy_wiki')
+    # print(f'tabs is {tabs}')
+    tab = next(tabs, None) # default value for generator to None instead of throwing StopIteration error
+    return tab
+
+def colour_tab(boss: Boss, tab) -> None:
+    colourOrange = int(to_color('orange', validate=True))
+    colourPink = int(to_color('pink', validate=True))
+    colourBlack = int(to_color('black', validate=True))
+
+    tab.active_fg = colourBlack
+    if tab.title == 'wiki':
+        tab.inactive_fg = colourOrange
+        tab.active_bg = colourOrange
+    else:
+        tab.inactive_fg = colourPink
+        tab.active_bg = colourPink
+    redraw_tab_bar(boss)
