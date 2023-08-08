3bcf72e8a946afe02d3b9e5269acc0552adf644e
tim goh <timslgoh@gmail.com>
2023-08-08 13:25:18 +0800

    refactor(vim/tg/misc): move unused function to utils

--- config/nvim/lua/core/utils/functions.lua
+++ config/nvim/lua/core/utils/functions.lua
@@ -10,4 +10,12 @@ M.close_floating_windows = function()
   end
 end
 
+-- unused
+function print_ex_command(command)
+  -- Get the current buffer
+  local bufnr = vim.api.nvim_get_current_buf()
+  -- Append the command to the buffer
+  vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, { vim.cmd.highlight })
+end
+
 return M
--- config/nvim/lua/tg/misc.lua
+++
@@ -1,6 +0,0 @@
-function print_ex_command(command)
-  -- Get the current buffer
-  local bufnr = vim.api.nvim_get_current_buf()
-  -- Append the command to the buffer
-  vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, { vim.cmd.highlight })
-end
