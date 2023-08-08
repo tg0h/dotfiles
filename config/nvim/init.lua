047e344b85fdd552d661cee938bf8b6471e1a043
tim goh <timslgoh@gmail.com>
2023-08-08 11:40:58 +0800

    refactor(vim/init): clean up init.lua

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,9 +1,6 @@
 -- require('core.diagnostic')
--- require('plugins')
 require('maps')
 require('core.options')
--- define colorscheme here
--- the colorscheme might set highlight clear, undoing all your highlight groups?
 require('core.autocmd')
 -- require('tg.toggle-interface')
 -- require('tg.toggle-test')
@@ -12,8 +9,6 @@ require('core.autocmd')
 -- require('tg.test-runner')
 -- require('tg.misc')
 -- require('tg.files')
--- require('setup.vim-kitty-navigator')
 
 -- Plugin management via lazy
 require('core.lazy')
--- require('core.colours')
