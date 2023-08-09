424bd2e3e7424c38c8b49c234ac03e389677259e
tim goh <timslgoh@gmail.com>
2023-08-09 11:50:46 +0800

    refactor(vim/init): cleanup

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -2,7 +2,6 @@
 require('maps')
 require('core.options')
 require('core.autocmd')
--- require('tg.telescope-multi-rg')
 require('core.utils.files') -- used by kitty tab switcher to open latest changed files in dir
 
 -- Plugin management via lazy
