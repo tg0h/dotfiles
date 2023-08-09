b8c94026739eafa2d7ad8295e09ac8a71c222f64
tim goh <timslgoh@gmail.com>
2023-08-09 10:50:53 +0800

    fix(vim/core): fix kitty typescript lang server error

--- config/nvim/lua/core/plugins/lsp.lua
+++ config/nvim/lua/core/plugins/lsp.lua
@@ -1,6 +1,14 @@
 local M = {
   'neovim/nvim-lspconfig',
   event = { 'BufReadPre', 'BufNewFile' },
+  -- the typescript lang server is installed in the mason bin directory
+  -- when opening nvim from the kitty tab switcher, need to set the bin folder in the kitty env.conf
+  -- env PATH=$PATH:/Users/tim/.local/share/nvim/mason/bin
+  -- set lazy to false to avoid Spawning language server with cmd: `typescript-language-server` failed. The language
+  -- server is either not installed, missing from PATH, or not executable. error when running tab switcher from kitty
+  -- hmm event = VeryLazy also works
+  -- lazy = 'false',
+  -- event = 'VeryLazy',
   dependencies = {
     { 'b0o/schemastore.nvim' },
     { 'hrsh7th/nvim-cmp' },
