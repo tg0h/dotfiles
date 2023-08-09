645e45bc56eeb350089014894a31f23e1fecc916
tim goh <timslgoh@gmail.com>
2023-08-09 13:08:12 +0800

    feat(vim/core): add spectre

---
+++ config/nvim/lua/core/plugins/spectre.lua
@@ -0,0 +1,45 @@
+return {
+  'nvim-pack/nvim-spectre',
+  event = { 'BufReadPre', 'BufNewFile' },
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+  },
+  config = function()
+    local wk = require('which-key')
+    wk.register({
+      R = { 'Replace' },
+    }, { prefix = '<leader>', mode = 'n', {} })
+  end,
+  -- enabled = vim.g.config.plugins.spectre.enable,
+  keys = {
+    {
+      '<leader>Rr',
+      function()
+        require('spectre').toggle()
+      end,
+      desc = 'Toggle search and replace',
+    },
+    {
+      '<leader>Rw',
+      function()
+        require('spectre').open_visual({ select_word = true })
+      end,
+      desc = 'Search current word',
+    },
+    {
+      '<leader>Rw',
+      mode = 'v',
+      function()
+        require('spectre').open_visual({ select_word = true })
+      end,
+      desc = 'Search current word',
+    },
+    {
+      '<leader>Rf',
+      function()
+        require('spectre').open_file_search({ select_word = true })
+      end,
+      desc = 'Search on current file',
+    },
+  },
+}
