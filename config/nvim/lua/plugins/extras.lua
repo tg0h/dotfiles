8a8d6724e72aa56f092ace57629792fcde49e53e
tim goh <timslgoh@gmail.com>
2023-08-12 13:49:06 +0800

    fix(vim/plugins): fix git blame showing immediately

--- config/nvim/lua/plugins/extras.lua
+++ config/nvim/lua/plugins/extras.lua
@@ -9,6 +9,9 @@ return {
   {
     'f-person/git-blame.nvim',
     event = 'VeryLazy',
+    init = function()
+      vim.g.gitblame_enabled = 0
+    end,
     config = function()
       local g = vim.g
       g.gitblame_enabled = 0
