979bb7d8eab193c319e91c7db2a77bb0c9dd5696
tim goh <timslgoh@gmail.com>
2023-08-13 17:10:49 +0800

    fix(vim/plugins): fix spectre which key menu

--- config/nvim/lua/plugins/spectre.lua
+++ config/nvim/lua/plugins/spectre.lua
@@ -4,7 +4,7 @@ return {
   dependencies = {
     'nvim-lua/plenary.nvim',
   },
-  config = function()
+  init = function()
     local wk = require('which-key')
     wk.register({
       R = { 'Replace' },
