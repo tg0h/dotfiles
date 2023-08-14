7733e7c580c73bfe0b32c0d6ae3380a7a1941243
tim goh <timslgoh@gmail.com>
2023-08-14 19:55:14 +0800

    feat(vim/plugins): leader n , opens yanky history

--- config/nvim/lua/plugins/yanky.lua
+++ config/nvim/lua/plugins/yanky.lua
@@ -24,7 +24,7 @@ return {
     end,
     keys = {
         -- stylua: ignore
-      { "<leader>p", function() require("telescope").extensions.yank_history.yank_history({ }) end, desc = "Open Yank History" },
+      { "<leader>n,", function() require("telescope").extensions.yank_history.yank_history({ }) end, desc = "Open Yank History" },
       { 'y', '<Plug>(YankyYank)', mode = { 'n', 'x' }, desc = 'Yank text' },
       { 'p', '<Plug>(YankyPutAfter)', mode = { 'n', 'x' }, desc = 'Put yanked text after cursor' },
       { 'P', '<Plug>(YankyPutBefore)', mode = { 'n', 'x' }, desc = 'Put yanked text before cursor' },
