979e2f09e5866066d6ed000fdd4d94c948aeef7e
tim goh <timslgoh@gmail.com>
2023-08-19 16:13:57 +0800

    feat(vim/plugins): use thin scrollbar for preview

--- config/nvim/lua/plugins/fzf-lua.lua
+++ config/nvim/lua/plugins/fzf-lua.lua
@@ -69,12 +69,12 @@ return {
           -- Only used with the builtin previewer:
           title = true, -- preview border title (file/buf)?
           title_pos = 'center', -- left|center|right, title alignment
-          scrollbar = 'float', -- `false` or string:'float|border'
+          scrollbar = 'border', -- `false` or string:'float|border'
           -- float:  in-window floating border
           -- border: in-border chars (see below)
           scrolloff = '-2', -- float scrollbar offset from right
           -- applies only when scrollbar = 'float'
-          scrollchars = { '█', '' }, -- scrollbar chars ({ <full>, <empty> }
+          scrollchars = { '│', '' }, -- scrollbar chars ({ <full>, <empty> }
           -- applies only when scrollbar = 'border'
           delay = 100, -- delay(ms) displaying the preview
           -- prevents lag on fast scrolling
@@ -174,6 +174,7 @@ return {
         ['--layout'] = 'reverse',
         ['--border'] = 'none',
         ['--separator'] = ' ', -- get rid of annoying border on search line
+        -- ['--scrollbar'] = 'a a',
         -- ['--header-first'] = '',
       },
       -- fzf '--color=' options (optional)
