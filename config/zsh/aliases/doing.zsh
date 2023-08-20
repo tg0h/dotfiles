77dc615b445450ccc28bc12a347cea7188569de7
tim goh <timslgoh@gmail.com>
2023-08-20 19:34:15 +0800

    refactor(alias/doing): remove doing aliases

--- config/zsh/aliases/doing.zsh
+++
@@ -1,7 +0,0 @@
-alias doa='doing'
-alias dot='doing today'
-alias dor='doing recent' # list recent entries
-alias dos='doing since'
-alias doss='doing show'
-alias doy='doing yesterday'
-alias dol='doing last' # show last entry
