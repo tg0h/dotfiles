db66de59968ed0c6f39d5c614a67f513e40ee805
tim goh <timslgoh@gmail.com>
2023-08-26 13:26:46 +0800

    feat(alias/pgcli): map p. aliases for pgcli

---
+++ config/zsh/aliases/pgcli.zsh
@@ -0,0 +1,10 @@
+# alias ma='mycli -d local' # connect to my timtest db
+alias p.='pgcli --dsn local' # connect to postgres local
+alias p,.='pgcli --dsn prod' # connect to postgres local
+# alias m,='mycli -d staging' # connect to candy db staging
+# alias m,.='mycli -d prod-read' # connect to candy db prod-read
+# alias m..='mycli -d prod' # connect to candy db prod
+#
+# alias mo='mycli -d staging-customer-tracks' # connect to customer tracks
+# alias moe='mycli -d prod-customer-tracks' # connect to customer tracks
+# alias dx='mycli -d pentel_dsn -e'
