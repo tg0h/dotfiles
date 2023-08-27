924a27fcf1809c3c02ee00b3f2f3df30319c306f
tim goh <timslgoh@gmail.com>
2023-08-27 12:00:04 +0800

    refactor(vim/keymaps): make keymaps canonical

--- config/nvim/lua/keymaps/quick-fix.lua
+++ config/nvim/lua/keymaps/quick-fix.lua
@@ -1,9 +1,9 @@
 -- my own vim function!
 -- quicker shortcuts since lsp goto def is called with A-i
-vim.keymap.set('n', '<A-q>', ":lua require('tg.quickfix').ToggleQFList(1)<CR>", { desc = 'toggle quickfix list' })
+vim.keymap.set('n', '<M-q>', ":lua require('tg.quickfix').ToggleQFList(1)<CR>", { desc = 'toggle quickfix list' })
 vim.keymap.set('n', '<S-A-;>', ":lua require('tg.quickfix').ToggleQFList(0)<CR>", { desc = 'toggle loclist' })
-vim.keymap.set('n', '<A-j>', ':cnext<CR>', { desc = 'next quickfix item' })
-vim.keymap.set('n', '<A-k>', ':cprev<CR>', { desc = 'previous quickfix item' })
+vim.keymap.set('n', '<M-j>', ':cnext<CR>', { desc = 'next quickfix item' })
+vim.keymap.set('n', '<M-k>', ':cprev<CR>', { desc = 'previous quickfix item' })
 
 vim.keymap.set('n', '<C-j>', ':lnext<CR>', { desc = 'next loclist item' })
 vim.keymap.set('n', '<C-k>', ':lprev<CR>', { desc = 'previous loclist item' })
