57e875751c1d982336a2ed95024ea395a10689e4
tim goh <timslgoh@gmail.com>
2023-08-27 13:32:00 +0800

    feat(vim/plugins): disable spectre in flash

--- config/nvim/lua/plugins/flash.lua
+++ config/nvim/lua/plugins/flash.lua
@@ -35,6 +35,7 @@ return {
         'cmp_menu',
         'noice',
         'flash_prompt',
+        'spectre_panel',
         function(win)
           -- exclude non-focusable windows
           return not vim.api.nvim_win_get_config(win).focusable
