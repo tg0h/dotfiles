62c7f763bb1d52dc1b1bb31fcb6bd9090c20042c
tim goh <timslgoh@gmail.com>
2023-09-10 16:09:33 +0800

    fix(vim/plugins): use relative path to worktree

--- config/nvim/lua/plugins/git-worktree.lua
+++ config/nvim/lua/plugins/git-worktree.lua
@@ -5,9 +5,12 @@ return {
   dependencies = { 'nvim-telescope/telescope.nvim' },
   keys = {
     -- { '<LEADER>t', ":lua require('telescope').load_extension('git-worktree')<CR>", desc = 'git worktree' },
-    { '<C-Esc>', ":lua require('git-worktree').switch_worktree('main')<CR>", desc = 'switch to main' },
-    { '<C-S-=>', ":lua require('git-worktree').switch_worktree('scripts')<CR>", desc = 'switch to scripts' }, -- C-+
-    { '<LEADER>t.', ":lua require('git-worktree').switch_worktree('scripts')<CR>", desc = 'switch to scripts' },
+    { '<C-Esc>', ":lua require('git-worktree').switch_worktree('../main')<CR>", desc = 'switch to main' },
+    { '<C-S-=>', ":lua require('git-worktree').switch_worktree('../scripts')<CR>", desc = 'switch to scripts' }, -- C-+
+    { '<C-9>', ":lua require('git-worktree').switch_worktree('../review')<CR>", desc = 'switch to review' }, -- C-+
+    { '<LEADER>t.', ":lua require('git-worktree').switch_worktree('../scripts')<CR>", desc = 'switch to scripts' },
+    { '<LEADER>tr', ":lua require('git-worktree').switch_worktree('../review')<CR>", desc = 'switch to review' },
+    { '<LEADER>tm', ":lua require('git-worktree').switch_worktree('../main')<CR>", desc = 'switch to main' },
     { '<LEADER>tt', ":lua require('telescope').extensions.git_worktree.git_worktrees()<CR>", desc = 'git worktrees' },
     { '<C-b>', ":lua require('telescope').extensions.git_worktree.git_worktrees()<CR>", desc = 'git worktrees' },
     { '<LEADER>th', ":lua require('telescope').extensions.git_worktree.create_git_worktree()<CR>", desc = 'create git worktrees' },
