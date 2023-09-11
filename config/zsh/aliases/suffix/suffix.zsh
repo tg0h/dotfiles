01d92b0325dbed8dc4d7e737ebe5103c69993d8c
tim goh <timslgoh@gmail.com>
2023-09-11 09:40:05 +0800

    refactor(alias/suffix): sort suffix alias

--- config/zsh/aliases/suffix/suffix.zsh
+++ config/zsh/aliases/suffix/suffix.zsh
@@ -1,17 +1,17 @@
 # open files with editor
 # eg test.txt enter becomes nvim test.txt
-alias -s txt=nvim
-alias -s json=nvim
-# alias -s zsh=nvim # interferes with alias zsh.h.zsh which runs the script
 # alias -s js=nvim
-alias -s ts=nvim
-alias -s rb=nvim
-alias -s yaml=nvim
-alias -s yml=nvim
-alias -s config=nvim
+# alias -s zsh=nvim # interferes with alias zsh.h.zsh which runs the script
 alias -s cfg=nvim
 alias -s conf=nvim
-alias -s skdhrc=nvim
-alias -s md=nvim
-alias -s sql=nvim
+alias -s config=nvim
+alias -s json=nvim
 alias -s lua=nvim
+alias -s md=nvim
+alias -s rb=nvim
+alias -s skdhrc=nvim
+alias -s sql=nvim
+alias -s ts=nvim
+alias -s txt=nvim
+alias -s yaml=nvim
+alias -s yml=nvim
