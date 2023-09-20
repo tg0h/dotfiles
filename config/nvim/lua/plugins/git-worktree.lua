37e4c0a1c65a54fed7bf25e0a7c77ee6f4b7a557
tim goh <timslgoh@gmail.com>
2023-09-20 08:47:49 +0800

    feat(vim/plugins): add feature worktree

--- config/nvim/lua/plugins/git-worktree.lua
+++ config/nvim/lua/plugins/git-worktree.lua
@@ -7,9 +7,13 @@ return {
     -- { '<LEADER>t', ":lua require('telescope').load_extension('git-worktree')<CR>", desc = 'git worktree' },
     { '<C-Esc>', ":lua require('git-worktree').switch_worktree('../main')<CR>", desc = 'switch to main' },
     { '<C-S-=>', ":lua require('git-worktree').switch_worktree('../scripts')<CR>", desc = 'switch to scripts' }, -- C-+
-    { '<C-9>', ":lua require('git-worktree').switch_worktree('../review')<CR>", desc = 'switch to review' }, -- C-+
+    -- kitty maps C-- to C-\
+    { '<C-Bslash>', ":lua require('git-worktree').switch_worktree('../feature')<CR>", desc = 'switch to feature' }, -- C-+
+    { '<C-9>', ":lua require('git-worktree').switch_worktree('../review')<CR>", desc = 'switch to review' },
+    { '<C-8>', ":lua require('git-worktree').switch_worktree('../fix')<CR>", desc = 'switch to fix' },
     { '<LEADER>t.', ":lua require('git-worktree').switch_worktree('../scripts')<CR>", desc = 'switch to scripts' },
     { '<LEADER>tr', ":lua require('git-worktree').switch_worktree('../review')<CR>", desc = 'switch to review' },
+    { '<LEADER>tf', ":lua require('git-worktree').switch_worktree('../fix')<CR>", desc = 'switch to fix' },
     { '<LEADER>tm', ":lua require('git-worktree').switch_worktree('../main')<CR>", desc = 'switch to main' },
     { '<LEADER>tt', ":lua require('telescope').extensions.git_worktree.git_worktrees()<CR>", desc = 'git worktrees' },
     { '<C-b>', ":lua require('telescope').extensions.git_worktree.git_worktrees()<CR>", desc = 'git worktrees' },
