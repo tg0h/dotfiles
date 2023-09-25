c483dbd622184ff07e5ccef0f0e07391fcdf8b9c
tim goh <timslgoh@gmail.com>
2023-09-26 07:32:19 +0800

    docs(alias/pgcli): fix doc

--- config/zsh/aliases/pgcli.zsh
+++ config/zsh/aliases/pgcli.zsh
@@ -1,6 +1,6 @@
 # alias ma='mycli -d local' # connect to my timtest db
 alias p.='pgcli --dsn local' # connect to postgres local
-alias p,.='pgcli --dsn prod' # connect to postgres local
+alias p,.='pgcli --dsn prod' # connect to postgres prod
 # alias m,='mycli -d staging' # connect to candy db staging
 # alias m,.='mycli -d prod-read' # connect to candy db prod-read
 # alias m..='mycli -d prod' # connect to candy db prod
