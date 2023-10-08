7459e07ef5c54c5dba19d582f6b89442e8b18c04
tim goh <timslgoh@gmail.com>
2023-10-08 11:29:10 +0800

    feat(vim/plugins): change previous jump to C-b

--- config/nvim/lua/plugins/luasnip.lua
+++ config/nvim/lua/plugins/luasnip.lua
@@ -79,7 +79,7 @@ local M = {
     vim.keymap.set({ 'i', 's' }, '<C-t>', function()
       if ls.expand_or_jumpable() then ls.expand_or_jump() end
     end, { silent = true })
-    vim.keymap.set({ 'i', 's' }, '<C-h>', function()
+    vim.keymap.set({ 'i', 's' }, '<C-b>', function()
       if ls.jumpable(-1) then ls.jump(-1) end
     end, { silent = true })
     vim.keymap.set({ 'i', 's' }, '<C-n>', function()
