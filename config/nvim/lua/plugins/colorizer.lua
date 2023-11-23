5a24c60611ef4b3bd11b774860db3b1b593af0a1
tim goh <timslgoh@gmail.com>
2023-11-23 08:05:01 +0800

    feat(vim/colorizer): show hsla colours

--- config/nvim/lua/plugins/colorizer.lua
+++ config/nvim/lua/plugins/colorizer.lua
@@ -3,25 +3,31 @@ return {
   'NvChad/nvim-colorizer.lua',
   -- event = 'VeryLazy', -- colorizer does not work if verylazy!
   -- priority = 9999,
-  config = function()
-    require('colorizer').setup()
-    -- lua feature - pcall means protected call
-    -- local status_ok, colorizer = pcall(require, "colorizer")
-    -- if not status_ok then
-    --   return
-    -- end
-    -- colorizer.setup({ "*" }, {
-
-    -- for all filetypes, enable all the options
-    -- require('colorizer').setup({ '*' }, {
-    --   RGB = true, -- #RGB hex codes
-    --   RRGGBB = true, -- #RRGGBB hex codes
-    --   RRGGBBAA = true, -- #RRGGBBAA hex codes
-    --   rgb_fn = true, -- CSS rgb() and rgba() functions
-    --   hsl_fn = true, -- CSS hsl() and hsla() functions
-    --   css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-    --   css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-    --   names = true, -- "Name" codes like Blue
-    -- })
-  end,
+  opts = {
+    filetypes = { '*' },
+    user_default_options = {
+      RGB = true, -- #RGB hex codes
+      RRGGBB = true, -- #RRGGBB hex codes
+      names = true, -- "Name" codes like Blue or blue
+      RRGGBBAA = false, -- #RRGGBBAA hex codes
+      AARRGGBB = false, -- 0xAARRGGBB hex codes
+      rgb_fn = false, -- CSS rgb() and rgba() functions
+      hsl_fn = true, -- CSS hsl() and hsla() functions
+      css = false, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+      css_fn = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+      -- Available modes for `mode`: foreground, background,  virtualtext
+      mode = 'background', -- Set the display mode.
+      -- Available methods are false / true / "normal" / "lsp" / "both"
+      -- True is same as normal
+      tailwind = false, -- Enable tailwind colors
+      -- parsers can contain values used in |user_default_options|
+      sass = { enable = false, parsers = { 'css' } }, -- Enable sass colors
+      virtualtext = '■',
+      -- update color values even if buffer is not focused
+      -- example use: cmp_menu, cmp_docs
+      always_update = false,
+    },
+    -- all the sub-options of filetypes apply to buftypes
+    buftypes = {},
+  },
 }
