067680467062e46c4f6c8b95cd9d29d50861693f
tim goh <timslgoh@gmail.com>
2024-01-29 09:51:01 +0800

    feat(alias/sql-migrate): point sql-migrate aliases to new config file

--- config/zsh/aliases/sql-migrate.zsh
+++ config/zsh/aliases/sql-migrate.zsh
@@ -1,12 +1,12 @@
 #TODO: ae does not work because sql-migrate filename has a dash in it ?
 
-alias sms='sql-migrate status -config=dbconfig-local.yml'
+alias sms='sql-migrate status -config=dbconfig.timg.yml'
 
-alias smr='sql-migrate redo -config=dbconfig-local.yml'
-alias smrd='sql-migrate redo -dryrun -config=dbconfig-local.yml'
+alias smr='sql-migrate redo -config=dbconfig.timg.yml'
+alias smrd='sql-migrate redo -dryrun -config=dbconfig.timg.yml'
 
-alias smu='sql-migrate up -config=dbconfig-local.yml'
-alias smud='sql-migrate up -dryrun -config=dbconfig-local.yml'
+alias smu='sql-migrate up -config=dbconfig.timg.yml'
+alias smud='sql-migrate up -dryrun -config=dbconfig.timg.yml'
 
-alias smd='sql-migrate down -config=dbconfig-local.yml -limit'
-alias smdd='sql-migrate down -dryrun -config=dbconfig-local.yml -limit'
+alias smd='sql-migrate down -config=dbconfig.timg.yml -limit'
+alias smdd='sql-migrate down -dryrun -config=dbconfig.timg.yml -limit'
