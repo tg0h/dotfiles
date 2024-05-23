4b19916491f042e1797fd86e2ba135fae859fc08
tim goh <timslgoh@gmail.com>
2024-05-23 14:06:52 +0800

    fix(vim/blame): fix desc

--- config/nvim/lua/plugins/blame.lua
+++ config/nvim/lua/plugins/blame.lua
@@ -1,7 +1,7 @@
 return {
   'FabijanZulj/blame.nvim',
   -- cmd = 'ToggleBlame',
-  keys = { { '<LEADER>gb', '<CMD>ToggleBlame<CR>', desc = { 'toggle blame' } } },
+  keys = { { '<LEADER>gb', '<CMD>ToggleBlame<CR>', desc = 'toggle blame' } },
   opts = {
     width = 40,
     date_format = '%Y-%m-%d %H:%M:%S',
