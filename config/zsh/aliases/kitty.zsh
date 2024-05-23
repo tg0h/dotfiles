7ff5af227a7682663d13b40ffd4e895752398e3b
tim goh <timslgoh@gmail.com>
2024-05-23 14:14:00 +0800

    feat(alias/kitty): ks goes to sharky

--- config/zsh/aliases/kitty.zsh
+++ config/zsh/aliases/kitty.zsh
@@ -1,3 +1,4 @@
 alias ksk='kitty +kitten show_key'
 alias kskm='kitty +kitten show_key -m kitty' # really show the keys - do this outside tmux
 alias icat="kitty +kitten icat"
+alias ks='kitten ssh timg@sharky'
