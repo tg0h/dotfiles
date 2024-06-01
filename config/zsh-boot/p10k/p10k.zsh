aa6352be0016df85bd9cdf2e47f50b621108789c
tim goh <timslgoh@gmail.com>
2024-06-01 11:06:47 +0800

    feat(zsh-boot/p10k): show time

--- config/zsh-boot/p10k/p10k.zsh
+++ config/zsh-boot/p10k/p10k.zsh
@@ -97,7 +97,7 @@
     # todo                    # todo items (https://github.com/todotxt/todo.txt-cli)
     # timewarrior             # timewarrior tracking status (https://timewarrior.net/)
     # taskwarrior             # taskwarrior task count (https://taskwarrior.org/)
-    # time                  # current time
+    time                  # current time
     # ip                    # ip address and bandwidth usage for a specified network interface
     # public_ip             # public IP address
     # proxy                 # system-wide http/https/ftp proxy
@@ -1547,15 +1547,15 @@
   #
   # ####################################[ time: current time ]####################################
   # # Current time color.
-  # typeset -g POWERLEVEL9K_TIME_FOREGROUND=66
+  typeset -g POWERLEVEL9K_TIME_FOREGROUND=66
   # # Format for the current time: 09:51:02. See `man 3 strftime`.
   # typeset -g POWERLEVEL9K_TIME_FORMAT='%D{%H:%M:%S}'
   # # If set to true, time will update when you hit enter. This way prompts for the past
   # # commands will contain the start times of their commands as opposed to the default
   # # behavior where they contain the end times of their preceding commands.
-  # typeset -g POWERLEVEL9K_TIME_UPDATE_ON_COMMAND=true
+  typeset -g POWERLEVEL9K_TIME_UPDATE_ON_COMMAND=true
   # # Custom icon.
-  # typeset -g POWERLEVEL9K_TIME_VISUAL_IDENTIFIER_EXPANSION=
+  typeset -g POWERLEVEL9K_TIME_VISUAL_IDENTIFIER_EXPANSION=
   # # Custom prefix.
   # # typeset -g POWERLEVEL9K_TIME_PREFIX='%fat '
   #
