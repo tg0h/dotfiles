dcc740bc8a0ab8e8d8a573868d7a5d2ba0cb0865
tim goh <timslgoh@gmail.com>
2024-06-01 11:05:39 +0800

    feat(vim/after): C-h runs rest nvim

--- config/nvim/after/ftplugin/http.lua
+++ config/nvim/after/ftplugin/http.lua
@@ -1,4 +1,5 @@
-vim.api.nvim_buf_set_keymap(0, 'n', '<C-Bslash>', '<CMD>Rest run<CR>', { desc = 'rest run' }) -- C--
+-- vim.api.nvim_buf_set_keymap(0, 'n', '<C-Bslash>', '<CMD>Rest run<CR>', { desc = 'rest run' }) -- C--
+vim.api.nvim_buf_set_keymap(0, 'n', '<C-h>', '<CMD>Rest run<CR>', { desc = 'rest run' })
 vim.api.nvim_buf_set_keymap(0, 'n', '<C-g>', '<CMD>Rest result prev<CR>', { desc = 'rest result next' })
 vim.api.nvim_buf_set_keymap(0, 'n', '<C-r>', '<CMD>Rest result next<CR>', { desc = 'rest result prev' })
 vim.api.nvim_buf_set_keymap(0, 'n', '<C-p>', '<CMD>Rest run last<CR>', { desc = 'rest run last' })
