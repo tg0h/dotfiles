55345f21cda0e1fb1810e122629333d837f1e68a
tim goh <timslgoh@gmail.com>
2024-06-17 10:45:08 +0800

    feat(alias/neovim-edit): add aliases to go to http files

---
+++ config/zsh/aliases/neovim-edit.zsh
@@ -0,0 +1,3 @@
+alias ncap="nvim $HOME/src/me/zsh/candy/api/candy.http"
+alias nsap="nvim $HOME/src/me/zsh/candy/shopify/shopify.http"
+alias nsapg="nvim $HOME/src/me/zsh/candy/shopify/shopify.graphql.http"
