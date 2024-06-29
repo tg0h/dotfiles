07bbea8dc3b8bffb2995c62d68c55f67ed412d81
tim goh <timslgoh@gmail.com>
2024-06-29 12:03:35 +0800

    feat(vim/blame): M-b toggles git blame

--- config/nvim/lua/plugins/blame.lua
+++ config/nvim/lua/plugins/blame.lua
@@ -1,10 +1,26 @@
 return {
   'FabijanZulj/blame.nvim',
   -- cmd = 'ToggleBlame',
-  keys = { { '<LEADER>gb', '<CMD>ToggleBlame<CR>', desc = 'toggle blame' } },
+  keys = { { '<M-b>', '<CMD>BlameToggle<CR>', desc = 'toggle blame' } },
   opts = {
-    width = 40,
     date_format = '%Y-%m-%d %H:%M:%S',
     virtual_style = 'float', -- or right_align
+    views = {
+      window = window_view,
+      virtual = virtual_view,
+      default = window_view,
+    },
+    merge_consecutive = false,
+    max_summary_width = 30,
+    colors = nil,
+    commit_detail_view = 'vsplit',
+    -- format_fn = formats.commit_date_author_fn,
+    mappings = {
+      commit_info = 'i',
+      stack_push = '<TAB>',
+      stack_pop = '<BS>',
+      show_commit = '<CR>',
+      close = { '<esc>', 'q' },
+    },
   },
 }
