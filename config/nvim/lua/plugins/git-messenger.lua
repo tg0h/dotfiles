b98541c869e2fd7ccf12107787e92e0a66caca49
tim goh <timslgoh@gmail.com>
2024-06-29 12:04:11 +0800

    feat(vim/git-messenger): M-C-b opens git messenger

--- config/nvim/lua/plugins/git-messenger.lua
+++ config/nvim/lua/plugins/git-messenger.lua
@@ -2,7 +2,7 @@ return {
   'rhysd/git-messenger.vim',
   dev = true, -- download the repo locally to remap the o key to , and  the O key to .
   keys = {
-    { '<M-b>', '<CMD>GitMessenger<CR>', desc = 'git messenger' },
+    { '<M-C-b>', '<CMD>GitMessenger<CR>', desc = 'git messenger' },
   },
   init = function()
     vim.g.git_messenger_always_into_popup = true -- always focus the popup window so that i can navigate commit history immediately with , and .
